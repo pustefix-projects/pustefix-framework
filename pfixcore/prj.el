62448ad8df2a515a4029df654b847994091ea9df
(noauthor) <noreply@pustefixframework.org>
2003-02-04 15:41:24 +0000

    This commit was manufactured by cvs2svn to create branch 'schlund'.

--- pfixcore/prj.el
+++
@@ -1,6 +0,0 @@
-(jde-project-file-version "1.0")
-(jde-set-variables
- '(jde-run-working-directory "./")
- '(jde-global-classpath (quote ("./src" "./src-jserv" "./build" "./res" "./lib" "./lib/jserv/jsdk.jar")))
- '(jde-make-working-directory "./")
- '(jde-sourcepath (quote ("./src" "./src-jserv"))))
