39331e1fb5d1f27f6ad0bcdbe6302032725969b4
jenstl <noreply@pustefixframework.org>
2003-03-27 12:24:29 +0000

    *** empty log message ***

--- pfixcore/src/de/schlund/pfixxml/testenv/RecordManager.java
+++ pfixcore/src/de/schlund/pfixxml/testenv/RecordManager.java
@@ -1,40 +1,17 @@
 package de.schlund.pfixxml.testenv;
 
-import com.icl.saxon.expr.NodeSetValue;
-import com.icl.saxon.om.DocumentBuilderFactoryImpl;
-import com.icl.saxon.om.NodeEnumeration;
-import com.icl.saxon.om.NodeInfo;
-
-import de.schlund.pfixxml.PfixServletRequest;
-import de.schlund.pfixxml.RequestParam;
-import de.schlund.pfixxml.SPDocument;
-import de.schlund.pfixxml.serverutil.ContainerUtil;
-import de.schlund.pfixxml.xpath.PFXPathEvaluator;
-
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-
-import org.apache.log4j.Category;
-
-import org.apache.xerces.dom.DocumentImpl;
-
-import org.apache.xml.serialize.OutputFormat;
-import org.apache.xml.serialize.XMLSerializer;
-
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.Text;
-
-import org.xml.sax.SAXException;
+import de.schlund.pfixxml.*;
+import de.schlund.pfixxml.serverutil.*;
+import de.schlund.pfixxml.xpath.*;
+import java.io.*;
+import javax.servlet.http.*;
+import javax.xml.parsers.*;
+import org.apache.log4j.*;
+import org.apache.xerces.dom.*;
+import org.apache.xml.serialize.*;
+import org.apache.xpath.*;
+import org.w3c.dom.*;
+import org.xml.sax.*;
 
 
 /**
@@ -48,15 +25,20 @@ public final class RecordManager {
 
     //~ Instance/static variables ..................................................................
 
-    private static Category     CAT                       = Category.getInstance(RecordManager.class.getName());
+    private static Category               CAT   = Category.getInstance(RecordManager.class.getName());
+    private static DocumentBuilderFactory dbfac = DocumentBuilderFactory.newInstance();
+    static {
+        dbfac.setNamespaceAware(true);
+        dbfac.setValidating(false);
+    }
+
     private static final String SESS_RECORDMODE_DIR       = "__RECORDMODE__";
     private static final String SESS_RECORD_COUNTER       = "__RECORDCOUNT__";
-    private static final String XPATH_RECORDDIR           = "/make/@record_dir";
-    private static final String XPATH_RECOREDALLOW        = "/make/@record_allowed";
-    private static final String DEFAULT_STYLESHEET        = "default.xsl";
+    private static final String ATTR_RECORDDIR            = "record_dir";
     private static final String REQ_PARAM_KEY_RECMODE     = "__recordmode";
     private static final String REQ_PARAM_VAL_RECMODE_OFF = "0";
     private static final String XML_PARAM_VAL_RECORDALLOW = "true";
+    private static final String DEFAULT_STYLESHEET        = "default.xsl";
     private String              recordBaseDir             = null;
     private boolean             recordAllowed             = false;
 
@@ -66,20 +48,16 @@ public final class RecordManager {
         if (CAT.isDebugEnabled()) {
             CAT.debug(this.getClass().getName() + " initializing");
         }
-        DocumentBuilderFactory dbfac = new DocumentBuilderFactoryImpl();
-        dbfac.setNamespaceAware(true);
-        dbfac.setValidating(false);
         DocumentBuilder db  = dbfac.newDocumentBuilder();
         Document        doc = db.parse(depxml);
         getConfigFromXML(doc);
         debug("RecordManager constructor end");
     }
-
+    
     //~ Methods ....................................................................................
 
     public final void tryRecord(PfixServletRequest preq, HttpServletResponse resp, 
-                                SPDocument resdoc, HttpSession session, ContainerUtil cutil)
-                         throws Exception {
+                                SPDocument resdoc, HttpSession session, ContainerUtil cutil) throws Exception {
         boolean recording_enabled = false;
         if (! recordAllowed) {
             if (CAT.isDebugEnabled()) {
@@ -116,7 +94,7 @@ public final class RecordManager {
                 // Look in the session if user is in record mode.
                 String dir = (String) cutil.getSessionValue(session, SESS_RECORDMODE_DIR);
                 recording_enabled = (dir == null || dir.equals(REQ_PARAM_VAL_RECMODE_OFF)
-                                    || dir.equals("")) ? false : true;
+                                     || dir.equals("")) ? false : true;
                 if (recording_enabled) {
                     if (CAT.isDebugEnabled())
                         CAT.debug("We are in record mode");
@@ -138,26 +116,25 @@ public final class RecordManager {
     public final boolean isRecordmodeAllowed() {
         return recordAllowed;
     }
-
+    
     public final String getRecordmodeBaseDir() {
         return recordBaseDir;
     }
-
+    
     public final String getTestcaseName(HttpSession session, ContainerUtil cutil) {
         String name = (String) cutil.getSessionValue(session, SESS_RECORDMODE_DIR);
         return name;
     }
-
+    
     private String tryFindNewDir(String testcasename, String basedir) throws RecordManagerException {
         File file = new File(basedir + "/" + testcasename);
         if (! file.exists()) {
             boolean ok = file.mkdirs();
             if (! ok) {
-                throw new RecordManagerException("Unable to create dir:" + basedir + "/"
-                                                 + testcasename, null);
+                throw new RecordManagerException("Unable to create dir:" + basedir + "/" + testcasename, null);
             }
         } else {
-            if(CAT.isDebugEnabled()) {
+            if (CAT.isDebugEnabled()) {
                 CAT.debug(basedir + "/" + testcasename + " exists!!!!!!");
             }
             int    i       = 0;
@@ -165,55 +142,31 @@ public final class RecordManager {
             while (file.exists()) {
                 String suffix = "_" + i++;
                 if (CAT.isDebugEnabled()) {
-                    CAT.debug(file.getName() + " exists. Trying " + basedir + "/" + testcasename
-                              + suffix);
+                    CAT.debug(file.getName() + " exists. Trying " + basedir + "/" + testcasename + suffix);
                 }
                 newname = testcasename + suffix;
                 file    = new File(basedir + "/" + testcasename + suffix);
             }
             boolean ok = file.mkdirs();
             if (! ok) {
-                throw new RecordManagerException("Unable to create dir:" + basedir + "/"
-                                                 + testcasename, null);
+                throw new RecordManagerException("Unable to create dir:" + basedir + "/" + testcasename, null);
             }
             return newname;
         }
         return testcasename;
     }
 
-    private void getConfigFromXML(Document doc) throws RecordManagerException {
-        try {
-            PFXPathEvaluator xpath   = new PFXPathEvaluator();
-            NodeSetValue     ns      = xpath.evaluateAsNodeSetValue(XPATH_RECOREDALLOW, doc);
-            NodeEnumeration  ne      = ns.enumerate();
-            String           allowed = null;
-            NodeInfo         ni      = null;
-            if (ne.hasMoreElements()) {
-                ni      = ne.nextElement();
-                allowed = ni.getStringValue();
-            } else {
-                CAT.warn("Unable to find record_allowed attribute! Setting record mode allowed to false");
-                recordAllowed = false;
-                return;
-                //throw new RecordManagerException("Unable to find recordmode allowed!", null);
-            }
-            recordAllowed = (allowed.toUpperCase().equals(XML_PARAM_VAL_RECORDALLOW.toUpperCase()))
-                                ? true : false;
-            ns            = xpath.evaluateAsNodeSetValue(XPATH_RECORDDIR, doc);
-            ne            = ns.enumerate();
-            String dir    = null;
-            if (ne.hasMoreElements()) {
-                ni  = ne.nextElement();
-                dir = ni.getStringValue();
-            } else {
-                CAT.warn("Unable to find recording directory! Setting record mode allowed to false!");
-                recordAllowed = false;
-                return;
-                //throw new RecordManagerException("Unable to find recording directory!", null);
-            }
+    private void getConfigFromXML(Document doc) throws Exception {
+        Element root = doc.getDocumentElement();
+        String  dir  = root.getAttribute(ATTR_RECORDDIR);
+        if (dir == null) {
+            CAT.warn("Unable to find recording directory! Setting record mode allowed to false!");
+            recordBaseDir = null;
+            recordAllowed = false;
+            return;
+        } else {
             recordBaseDir = dir;
-        } catch (Exception e) {
-            throw new RecordManagerException("Error: " + e.getMessage(), e);
+            recordAllowed = true;
         }
     }
 
@@ -223,8 +176,7 @@ public final class RecordManager {
             if (msg == null) {
                 msg = "";
             }
-            sb.append("\n").append(this.getClass().getName() + " status: ").append("(").append(msg).append(
-                    ")").append("\n");
+            sb.append("\n").append(this.getClass().getName() + " status: ").append("(").append(msg).append(")").append("\n");
             sb.append("  recordDir          = " + recordBaseDir).append("\n");
             sb.append("  recordAllowed      = " + recordAllowed).append("\n");
             CAT.debug(sb.toString());
@@ -234,17 +186,15 @@ public final class RecordManager {
     private void debugSession(HttpSession session, ContainerUtil cutil, String msg) {
         if (CAT.isDebugEnabled()) {
             StringBuffer sb = new StringBuffer();
-            sb.append("\n").append(this.getClass().getName() + " session status: ").append("(").append(
-                    msg).append(")").append("\n");
-            sb.append("  SESS_RECORD_DIR   = "
-                      + cutil.getSessionValue(session, SESS_RECORDMODE_DIR)).append("\n");
+            sb.append("\n").append(this.getClass().getName() + " session status: ").
+                append("(").append(msg).append(")").append("\n");
+            sb.append("  SESS_RECORD_DIR   = " + cutil.getSessionValue(session, SESS_RECORDMODE_DIR)).append("\n");
             Integer integer = (Integer) cutil.getSessionValue(session, SESS_RECORD_COUNTER);
-            sb.append("  SESS_RECORD_COUNT = "
-                      + ((integer == null) ? "null" : ("" + integer.intValue()))).append("\n");
+            sb.append("  SESS_RECORD_COUNT = " + ((integer == null) ? "null" : ("" + integer.intValue()))).append("\n");
             CAT.debug(sb.toString());
         }
     }
-
+    
     /**
      * Start recording. All requests and results will be logged to file.
      * @param count a number which will be appended to the filename used to
@@ -261,7 +211,7 @@ public final class RecordManager {
                                   PfixServletRequest pfix_servlet_request, 
                                   SPDocument result_document) throws RecordManagerException {
 
-        if(CAT.isDebugEnabled()) {
+        if (CAT.isDebugEnabled()) {
             StringBuffer sb = new StringBuffer(512);
             sb.append("\nRecordManager#doRecord current PfixServletRequest: \n"); 
             sb.append("URI: " + uri).append("\n");
@@ -383,22 +333,20 @@ public final class RecordManager {
             }
             file = new File(new_filename);
         }
-        OutputFormat  out_format = new OutputFormat("xml", "ISO-8859-1", true);
-        XMLSerializer ser = new XMLSerializer();
-        ser.setOutputFormat(out_format);
+        OutputFormat     out_format = new OutputFormat("xml", "ISO-8859-1", true);
         out_format.setIndent(2);
         out_format.setPreserveSpace(false);
+        FileOutputStream out_stream;
         try {
-            ser.setOutputCharStream(new FileWriter(file));
-        } catch (IOException e) {
-            throw new RecordManagerException("IOException occured during serialization!: " + file, 
-                                             e);
+            out_stream = new FileOutputStream(file);
+        } catch (FileNotFoundException e) {
+            throw new RecordManagerException("IOException occured during serialization!: " + file, e);
         }
+        XMLSerializer ser = new XMLSerializer(out_stream, out_format);
         try {
             ser.serialize(doc);
         } catch (IOException e) {
-            throw new RecordManagerException("IOException occured during serialization!: " + file, 
-                                             e);
+            throw new RecordManagerException("IOException occured during serialization!: " + file, e);
         }
     }
 }
