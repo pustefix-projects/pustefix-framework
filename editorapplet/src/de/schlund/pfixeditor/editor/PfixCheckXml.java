6ba729e389536aa854ec1081e0d18656958e1b7c
zaich <noreply@pustefixframework.org>
2003-05-05 14:47:18 +0000

    *** empty log message ***

---
+++ editorapplet/src/de/schlund/pfixeditor/editor/PfixCheckXml.java
@@ -0,0 +1,55 @@
+package de.schlund.pfixeditor.editor;
+
+import javax.swing.event.*; 
+import java.awt.AWTPermission;
+import java.awt.Frame;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.*;
+import java.lang.Object.*;
+import java.awt.Font;
+import java.awt.event.*;
+import java.util.*;
+
+import de.schlund.pfixeditor.xml.*;
+
+import javax.swing.text.*;
+import java.io.*;
+import java.util.*;
+
+
+
+import javax.swing.*;
+
+
+public class PfixCheckXml {
+
+
+    JTextComponent textpane;
+
+    public PfixCheckXml(JTextComponent pane) {
+        textpane = pane;
+        
+    }
+
+    
+    public boolean checkXML() {
+
+        boolean wellformed;
+        boolean result=false;
+	ArrayList xmlErrors=null;
+        XmlError xErr=null;
+        XmlTextProcessor xmlProc=new XmlTextProcessor();
+        result=xmlProc.parse("<foo>"+this.textpane.getText()+"</foo>");
+        xmlErrors=xmlProc.getXmlErrors(); 
+        if (result) {
+            wellformed = true;
+        }
+        else {
+            wellformed = false;
+        }
+        return wellformed;
+	
+    }
+    
+}
