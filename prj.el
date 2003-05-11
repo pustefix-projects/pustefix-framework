22420fd09a90e1ba6043a9b7ab032c5e2291b217
jenstl <noreply@pustefixframework.org>
2003-05-11 23:34:26 +0000

    *** empty log message ***

--- prj.el
+++ prj.el
@@ -3,4 +3,5 @@
  '(jde-sourcepath (quote ("./src" "./src-jserv")))
  '(jde-make-working-directory "./")
  '(jde-global-classpath (quote ("./src" "./src-jserv" "./build" "./res" "./lib" "./lib/jserv/jsdk.jar")))
- '(jde-run-working-directory "./"))
+ '(jde-run-working-directory "./")
+ '(jde-compile-option-directory "./build"))
