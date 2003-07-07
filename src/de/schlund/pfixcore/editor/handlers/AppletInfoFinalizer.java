2b3370749486b899a12895630cabd7fd45ab59d1
zaich <noreply@pustefixframework.org>
2003-07-07 15:19:56 +0000

    *** empty log message ***

--- src/de/schlund/pfixcore/editor/handlers/AppletInfoFinalizer.java
+++ src/de/schlund/pfixcore/editor/handlers/AppletInfoFinalizer.java
@@ -1,83 +1,84 @@
-package de.schlund.pfixcore.editor.handlers;
+/*
+* This file is part of PFIXCORE.
+*
+* PFIXCORE is free software; you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* PFIXCORE is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public License
+* along with PFIXCORE; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*
+*/
 
+package de.schlund.pfixcore.editor.handlers;
 import de.schlund.pfixcore.editor.*;
+import de.schlund.pfixcore.editor.auth.ProjectPermissions;
 import de.schlund.pfixcore.editor.interfaces.*;
 import de.schlund.pfixcore.editor.resources.*;
 import de.schlund.pfixcore.generator.*;
 import de.schlund.pfixcore.workflow.*;
 import de.schlund.pfixcore.workflow.Navigation.*;
 import de.schlund.pfixcore.workflow.app.*;
-
 import de.schlund.pfixxml.*;
 import de.schlund.pfixxml.targets.*;
-
 import de.schlund.util.*;
 import de.schlund.util.statuscodes.*;
-
 import java.util.*;
+
+import org.apache.xpath.XPathAPI;
+import org.w3c.dom.*;
 import java.io.*;
 
-import org.apache.xpath.*;
+import javax.xml.transform.TransformerException;
 
-import org.w3c.dom.*;
-
-import de.schlund.pfixcore.workflow.*;
-import de.schlund.pfixcore.util.*;
-import de.schlund.util.FactoryInit;
-import de.schlund.pfixxml.*;
-import de.schlund.pfixxml.targets.*;
-import java.util.*;
-import org.apache.log4j.*;
-import org.w3c.dom.*;
-import org.apache.xml.serialize.*;
-import java.io.*;
-import org.xml.sax.*;
-import javax.xml.parsers.*;
-import org.apache.xpath.*;
 
 /**
- * @author zaich
+ * AppletInfoFinalizer.java
  *
- * To change this generated comment edit the template variable "typecomment":
- * Window>Preferences>Java>Templates.
+ *
+ * Created: Fri Jul 04 16:40:07 2003
+ *
+ * @author <a href="mailto:zaich@schlund.de">Volker Zaich</a>
+ * @version
+ *
+ *
  */
+
+
 public class AppletInfoFinalizer extends ResdocSimpleFinalizer {
 
 	private String currdoc = null;
-	private static DocumentBuilderFactory dbfac =
-		DocumentBuilderFactory.newInstance();
 
 	protected void renderDefault(IWrapperContainer container) throws Exception {
-
-
-                System.out.println("Hallo -- Ich bin drin");
-
             
-		Context context = container.getAssociatedContext();
-		ContextResourceManager crm = context.getContextResourceManager();
-		EditorSessionStatus esess = EditorRes.getEditorSessionStatus(crm);
-		ResultDocument resdoc = container.getAssociatedResultDocument();		
-		// TargetGenerator tgen = eprod.getTargetGenerator();
-
-                System.out.println("Hallo -- Ich bin drin");
+            Context                context     = container.getAssociatedContext();
+            ContextResourceManager crm         = context.getContextResourceManager();
+            EditorSessionStatus    esess       = EditorRes.getEditorSessionStatus(crm);
+            EditorSearch           esearch     = EditorRes.getEditorSearch(crm);
+            ResultDocument         resdoc      = container.getAssociatedResultDocument();
+            TargetGenerator        tgen        = esess.getProduct().getTargetGenerator();
+            AuxDependency          currinclude = esess.getCurrentInclude();
+            PfixcoreNamespace[]    nspaces     = esess.getProduct().getPfixcoreNamespace();
+                                                                      
+            for (int i = 0; i < nspaces.length; i++) {              
+                    PfixcoreNamespace nsp = nspaces[i];
+                    resdoc.addUsedNamespace(nsp.getPrefix(), nsp.getUri());
+            }
                 
-		// currdoc = esess.getCurrentDocumentationId();
-
-		// esess.insertStatus(resdoc, resdoc.createNode("cr_editorsession"));
-
-		Element root = resdoc.createNode("WeihnachtsmanninBlau");
-
-		// String[] id = eprod.getDocumentation().getDocumentationIds();
-		// String[] values = eprod.getDocumentation().getDocumentationValues();
-
-		// this.renderAllDocumentation(id, root, container, values);
-
-
-                  Element neu = resdoc.createSubNode(root, "stylesheet");
-			neu.setAttribute("Bla", "Blabla");
-                        
-
+            // Render the current status of the editor session
+            esess.insertStatus(resdoc, resdoc.createNode("cr_editorsession"));
                 
+            // Render all includes
+            TreeSet includes = tgen.getDependencyRefCounter().getDependenciesOfType(DependencyType.TEXT);
+            Element root     = resdoc.createNode("allincludes"); 
+            EditorHelper.renderIncludesForAppletInfo(includes, resdoc, root);                                        
 
 	}
 
@@ -86,7 +87,6 @@ public class AppletInfoFinalizer extends ResdocSimpleFinalizer {
 
     	public void onSuccess(IWrapperContainer container) throws Exception {
 		renderDefault(container);
-                System.out.println("Blaaaa on Succes");
 	}
 
 
