ffdbbc62488a8a72f640b07a734330916bd892bb
haexel <noreply@pustefixframework.org>
2003-10-17 17:08:54 +0000

    *** empty log message ***

--- pfixcore/src/de/schlund/pfixxml/CacheStatisticServlet.java
+++ pfixcore/src/de/schlund/pfixxml/CacheStatisticServlet.java
@@ -24,24 +24,54 @@ import de.schlund.pfixxml.testenv.XMLSerializeUtil;
  */
 public class CacheStatisticServlet extends HttpServlet {
 
+    private static int OUTPUTXML=0;
+    private static int OUTPUTTEXT=1;
     /**
 	 * @see javax.servlet.http.HttpServlet#doGet(javax.servlet.http.HttpServletRequest,
 	 *      javax.servlet.http.HttpServletResponse)
 	 */
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-
-        Document doc;
-        try {
-            doc = SPCacheStatistic.getInstance().getXMLCacheStatistic();
-        } catch (ParserConfigurationException e) {
-            throw new ServletException(e);
+        int outmode = OUTPUTXML;
+        
+        String outputmode_param = req.getParameter("mode");
+      
+        if(outputmode_param != null) {
+            if(outputmode_param.equals("text")) {
+                outmode = OUTPUTTEXT;
+            }
         }
-
-        String str = XMLSerializeUtil.getInstance().serializeToString(doc);
-
-        res.setContentType("text/xml");
+        boolean reset = false;
+        
+        String reset_param = req.getParameter("doreset");
+        if(reset_param != null) {
+            if(reset_param.equals("true")) {
+                reset = true;
+            }
+        }
+        
+        //////////////////
+        if(reset) {
+            SPCacheStatistic.getInstance().reset();
+        }
+        String output;
+        
+        if(outmode == OUTPUTXML) {
+            Document doc;
+            try {
+                doc = SPCacheStatistic.getInstance().getCacheStatisticAsXML();
+            } catch (ParserConfigurationException e) {
+                throw new ServletException(e);
+            }
+            res.setContentType("text/xml");
+            output = XMLSerializeUtil.getInstance().serializeToString(doc);
+        } else if(outmode == OUTPUTTEXT) {
+            output = SPCacheStatistic.getInstance().getCacheStatisticAsString(); 
+            res.setContentType("text/text");
+        } else {
+            throw new ServletException("No outputmode set.");
+        }
         ServletOutputStream ostream = res.getOutputStream();
-        ostream.println(str);
+        ostream.println(output);
     }
 
     /**
--- pfixcore/src/de/schlund/pfixxml/targets/SPCacheStatistic.java
+++ pfixcore/src/de/schlund/pfixxml/targets/SPCacheStatistic.java
@@ -4,34 +4,50 @@
  */
 package de.schlund.pfixxml.targets;
 
+import java.io.File;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Properties;
 
+import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
+import org.apache.xpath.XPathAPI;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Text;
+
+import de.schlund.pfixxml.XMLException;
+import de.schlund.util.FactoryInit;
 
 /**
  * @author Joerg Haecker <haecker@schlund.de>
  *
  */
-public class SPCacheStatistic {
+public class SPCacheStatistic implements FactoryInit {
 
     private HashMap targetGenStatistic;
+    private HashMap dependXMLToProductnameMapping;
     private static SPCacheStatistic theInstance = new SPCacheStatistic();
     private static int REGISTER_MISS = 0;
     private static int REGISTER_HIT = 1;
     private static DocumentBuilderFactory dbfac = DocumentBuilderFactory.newInstance();
+    private static String PROP_PRODUCTDATA = "editorproductfactory.productdata";
     private long allHits = 0;
     private long allMisses = 0;
+    private String productsconf;
+    private DecimalFormat hitrateFormat = new DecimalFormat("##0.00");
+    
 
     private SPCacheStatistic() {
         targetGenStatistic = new HashMap();
+        dependXMLToProductnameMapping = new HashMap();
     }
 
     public static SPCacheStatistic getInstance() {
@@ -52,7 +68,9 @@ public class SPCacheStatistic {
     /**
      * Create cache-statistic in XML-format.
      */
-    synchronized public Document getXMLCacheStatistic() throws ParserConfigurationException {
+     public Document getCacheStatisticAsXML() throws ParserConfigurationException {
+        HashMap targetgentoinfomap_clone = targetgentoinfomap_clone = cloneTargetGeneratorToCacheHitMissInfoMapping();
+         
         Document doc = dbfac.newDocumentBuilder().newDocument();
         Element top = doc.createElement("spcachestatistic");
 
@@ -64,8 +82,8 @@ public class SPCacheStatistic {
         
         TargetsInSPCache targetsincache = getTargetsForTargetGeneratorFromSPCache();
 
-        Element ele_hitmiss = doc.createElement("targetgenerators");
-        attachTargetGenerators(doc, targetsincache, ele_hitmiss);
+        Element ele_hitmiss = doc.createElement("products");
+        attachTargetGenerators(doc, targetsincache, ele_hitmiss, targetgentoinfomap_clone);
         attachShared(doc, targetsincache, ele_hitmiss);
         top.appendChild(ele_hitmiss);
 
@@ -73,18 +91,51 @@ public class SPCacheStatistic {
         
         return doc;     
     }
-
-    /* Attach all TargetGenerators */
-    private void attachTargetGenerators(Document doc, TargetsInSPCache targetsincache, Element ele_hitmiss) {
-        DecimalFormat df = new DecimalFormat("##0.00");
-        for(Iterator i = targetGenStatistic.keySet().iterator(); i.hasNext(); ) {
+   
+     public String getCacheStatisticAsString() {
+        StringBuffer sb= new StringBuffer(128);
+        SPCache currentcache = SPCacheFactory.getInstance().getCache();
+        sb.append("Cache class="+currentcache.getClass().getName()+" capacity="+currentcache.getCapacity()+
+                  " size="+currentcache.getSize()+" hits="+allHits+" misses="+allMisses+
+                  " hitrate="+formatHitrate((double)allHits, (double)allMisses)+"|");
+        
+        HashMap targetgentoinfomap_clone = cloneTargetGeneratorToCacheHitMissInfoMapping();
+       
+        for(Iterator i = targetgentoinfomap_clone.keySet().iterator(); i.hasNext(); ) {
             TargetGenerator tgen = (TargetGenerator) i.next();
-            Element ele_tg = doc.createElement("targetgenerator");
+            CacheHitMissPair hitmiss = (CacheHitMissPair) targetgentoinfomap_clone.get(tgen);
+            sb.append("|product name="+getProductnameForTargetGenerator(tgen)+
+                        " hitrate="+formatHitrate((double) hitmiss.hits, (double) hitmiss.misses)+
+                        " hits="+hitmiss.hits+" misses="+hitmiss.misses);
+        }
+        
+        return sb.toString();
+    }
+   
+     
+     
+    private HashMap cloneTargetGeneratorToCacheHitMissInfoMapping() {
+        HashMap targetgentoinfomap_clone;
+        synchronized(this) {
+            targetgentoinfomap_clone = (HashMap) targetGenStatistic.clone();
+        }
+        return targetgentoinfomap_clone;
+    }
+
+   
+    /* Attach all TargetGenerators */
+    private void attachTargetGenerators(Document doc, TargetsInSPCache targetsincache, Element ele_hitmiss, HashMap targetgentoinfomap) {
+        
+        for(Iterator i = targetgentoinfomap.keySet().iterator(); i.hasNext(); ) {
+            TargetGenerator tgen = (TargetGenerator) i.next();
+            Element ele_tg = doc.createElement("product");
             
-            ele_tg.setAttribute("config", tgen.getConfigname());    
-            CacheHitMissPair hitmiss = (CacheHitMissPair) targetGenStatistic.get(tgen);
-            double rate = ((double)hitmiss.hits / (hitmiss.hits + hitmiss.misses)) * 100;
-            ele_tg.setAttribute("hitrate", df.format(rate)+"%");
+            String dependxml;
+            String configattr = getProductnameForTargetGenerator(tgen);
+            
+            ele_tg.setAttribute("name", configattr);    
+            CacheHitMissPair hitmiss = (CacheHitMissPair) targetgentoinfomap.get(tgen);
+            ele_tg.setAttribute("hitrate", formatHitrate((double) hitmiss.hits, (double) hitmiss.misses));
             ele_tg.setAttribute("hits", ""+hitmiss.hits);
             ele_tg.setAttribute("misses", ""+hitmiss.misses);
         
@@ -92,7 +143,31 @@ public class SPCacheStatistic {
             ele_hitmiss.appendChild(ele_tg);
         }
     }
+    
+    private String getProductnameForTargetGenerator(TargetGenerator tgen) {
+        String dependxml = tgen.getConfigname();
+        String configattr;
+        if(dependXMLToProductnameMapping.containsKey(dependxml)) {
+            configattr = (String) dependXMLToProductnameMapping.get(dependxml);
+        } else {
+            String configname = tgen.getConfigname();
+            configattr = configname.substring(tgen.getConfigname().indexOf(tgen.getDocroot()), configname.length());
+        }
+        return configattr;
+    }
 
+    private String formatHitrate(double hits, double misses) {
+        double rate = 0;
+        if(hits != 0 && misses != 0) {
+            rate = (hits / (misses + hits)) * 100;
+            if(rate > 100) {
+                rate = 100;
+            }
+        }
+                
+        return hitrateFormat.format(rate)+"%";
+    }
+    
     /* Attach all SharedLeafs */
     private void attachShared(Document doc, TargetsInSPCache targetsincache, Element ele_hitmiss) {
         if(! targetsincache.sharedTargets.isEmpty()) {
@@ -126,6 +201,7 @@ public class SPCacheStatistic {
         ele_currentcache.setAttribute("size", ""+currentcache.getSize());
         ele_currentcache.setAttribute("hits", ""+allHits);
         ele_currentcache.setAttribute("misses", ""+allMisses);
+        ele_currentcache.setAttribute("hitrate", formatHitrate((double)allHits, (double)allMisses));
     }
 
     
@@ -174,7 +250,38 @@ public class SPCacheStatistic {
             targetGenStatistic.put(tgen, hitmiss);
         }
     }
+
+    /**
+     * @see de.schlund.util.FactoryInit#init(java.util.Properties)
+     */
+    public void init(Properties props) throws Exception {
+        String productdatafile = props.getProperty(PROP_PRODUCTDATA);
+        if(productdatafile == null || productdatafile.equals("")) {
+            throw new XMLException("Need property '"+PROP_PRODUCTDATA+"' for retrieving product data.");
+        }
+        DocumentBuilder docbuilder = dbfac.newDocumentBuilder();
+        Document doc = docbuilder.parse(new File(productdatafile));
+        
+        NodeList nl = XPathAPI.selectNodeList(doc, "/projects/project");
+        for(int i=0; i<nl.getLength(); i++) {
+            Node project_node = nl.item(i);
+            String prjname = ((Element) project_node).getAttribute("name");
+            Node dependxml_node = XPathAPI.selectSingleNode(project_node, "./depend");
+            String dependname = ((Text)((Element) dependxml_node).getFirstChild()).getData();
+            dependXMLToProductnameMapping.put(dependname, prjname);
+        }
+    }
     
+    public void reset() {
+        allHits = 0;
+        allMisses = 0;
+        HashMap targetgentoinfomap_clone = cloneTargetGeneratorToCacheHitMissInfoMapping();
+        for(Iterator i = targetgentoinfomap_clone.keySet().iterator(); i.hasNext(); ) {
+            CacheHitMissPair misshit = (CacheHitMissPair) targetgentoinfomap_clone.get(i.next());
+            misshit.misses = 0;
+            misshit.hits = 0;
+        }
+    }
    
 }
 
