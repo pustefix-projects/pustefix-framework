fb1d09f446c0645533063a06eddb265ad7dad228
jenstl <noreply@pustefixframework.org>
2004-02-11 12:56:00 +0000

    *** empty log message ***

--- pfixcore/prj.el
+++ pfixcore/prj.el
@@ -2,7 +2,7 @@
 (jde-set-variables
  '(jde-sourcepath (quote ("./src")))
  '(jde-make-working-directory "./")
- '(jde-global-classpath (quote ("./src" "./build" "./res" "./lib" "./example/servletconf/tomcat/common/lib/servlet.jar")))
+ '(jde-global-classpath (quote ("/opt/apache-ant-1.5.4/lib/optional.jar" "./src" "./build" "./res" "./lib" "./example/servletconf/tomcat/common/lib/servlet.jar")))
  '(jde-run-working-directory "./")
  '(jde-compile-option-directory "./build")
  '(jde-built-class-path (quote ("./build"))))
