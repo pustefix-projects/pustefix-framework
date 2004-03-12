da0b439de3b05b522060202497595f9675dcf02d
(noauthor) <noreply@pustefixframework.org>
2004-03-12 17:51:39 +0000

    This commit was manufactured by cvs2svn to create tag 'AUTO_TAG_2004-05-07-152719'.

--- pfixcore/prj.el
+++
@@ -1,8 +0,0 @@
-(jde-project-file-version "1.0")
-(jde-set-variables
- '(jde-make-working-directory "./")
- '(jde-global-classpath (quote ("/opt/apache-ant-1.5.4/lib/optional.jar" "./src" "./build" "./res" "./lib" "./example/servletconf/tomcat/common/lib/servlet.jar")))
- '(jde-run-working-directory "./")
- '(jde-compile-option-directory "./build")
- '(jde-sourcepath (quote ("./src")))
- '(jde-built-class-path (quote ("./build"))))
--- pfixcore/skel/apploader.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-export CLASSPATH=`ls -1 lib/pfixcore-*.jar`
-java -Dapploader.script="$0" de.schlund.pfixxml.loader.CommandClient $@
