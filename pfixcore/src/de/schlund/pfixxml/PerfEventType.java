5ba253cc3101a568dc87839e10cbe7450426ddb1
haexel <noreply@pustefixframework.org>
2004-07-01 12:40:59 +0000

    *** empty log message ***

--- pfixcore/src/de/schlund/pfixxml/PerfEventType.java
+++ pfixcore/src/de/schlund/pfixxml/PerfEventType.java
@@ -16,27 +16,91 @@ import de.schlund.pfixcore.workflow.PageRequest;
  */
 public class PerfEventType {
     
-    public static final PerfEventType IHANDLER_ISACTIVE = new PerfEventType("IHANDLER_IS_ACTIVE", 3);
-    public static final PerfEventType IHANDLER_NEEDSDATA = new PerfEventType("IHANDLER_NEEDS_DATA", 3);
-    public static final PerfEventType IHANDLER_PREREQUISITESMET = new PerfEventType("IHANDLER_PREREQUISITES_MET", 3);
+    //TODO: this is clumsy. What happens if this class is loaded before
+    // factory init (PerfTypeConfig) is finished??? Can this happen?
+    private static final String IHANDLER_ISACTIVE_TAG = "IHANDLER_IS_ACTIVE";
+    public static final PerfEventType IHANDLER_ISACTIVE = 
+        new PerfEventType(IHANDLER_ISACTIVE_TAG, 
+                PerfEventTypeConfig.getInstance().getPerfDelayProperty(IHANDLER_ISACTIVE_TAG));
     
-    public static final PerfEventType CONTEXTRESOURCE_INSERTSTATUS = new PerfEventType("CONTEXTRESOURCE_INSERT_STATUS", 5);
-    public static final PerfEventType CONTEXTRESOURCE_OBSERVERUPDATE = new PerfEventType("CONTEXTRESOURCE_OBSERVER_UPDATE", 0);
+    private static final String IHANDLER_NEEDSDATA_TAG = "IHANDLER_NEEDS_DATA";
+    public static final PerfEventType IHANDLER_NEEDSDATA = 
+        new PerfEventType(IHANDLER_NEEDSDATA_TAG, 
+                PerfEventTypeConfig.getInstance().getPerfDelayProperty(IHANDLER_NEEDSDATA_TAG));
     
-    public static final PerfEventType PAGE_ISACCESSIBLE = new PerfEventType("PAGE_IS_ACCESSIBLE", 10);
-    public static final PerfEventType PAGE_NEEDSDATA = new PerfEventType("PAGE_NEEDS_DATA", 10);
-    public static final PerfEventType PAGE_HANDLESUBMITTEDDATA = new PerfEventType("PAGE_HANDLE_SUBMITTED_DATA", 300);
-    public static final PerfEventType PAGE_INITIWRAPPERS = new PerfEventType("PAGE_INIT_IWRAPPERS", 5);
-    public static final PerfEventType PAGE_RETRIEVECURRENTSTATUS = new PerfEventType("PAGE_RETRIEVE_CURRENT_STATUS", 5);
+    private static final String IHANDLER_PREREQUISITESMET_TAG = "IHANDLER_PREREQUISITES_MET";
+    public static final PerfEventType IHANDLER_PREREQUISITESMET = 
+        new PerfEventType(IHANDLER_PREREQUISITESMET_TAG, 
+                PerfEventTypeConfig.getInstance().getPerfDelayProperty(IHANDLER_PREREQUISITESMET_TAG));
     
-    public static final PerfEventType CONTEXT_CREATENAVICOMPLETE = new PerfEventType("CONTEXT_CREATE_NAVI_COMPLETE", 25);
-    public static final PerfEventType CONTEXT_CREATENAVIREUSE = new PerfEventType("CONTEXT_CREATE_NAVI_REUSE", 2);
     
-    public static final PerfEventType XMLSERVER_HANDLEDOCUMENT = new PerfEventType("XMLSERVER_HANDLE_DOCUMENT", 100);
-    public static final PerfEventType XMLSERVER_GETDOM = new PerfEventType("XMLSERVER_GET_DOM", 400);
-    public static final PerfEventType XMLSERVER_CALLPROCESS= new PerfEventType("XMLSERVER_CALL_PROCESS", 0);
-    public static final PerfEventType XMLSERVER_PREPROCESS = new PerfEventType("XMLSERVER_PRE_PROCESS", 0);
+    private static final String CONTEXTRESOURCE_INSERTSTATUS_TAG = "CONTEXTRESOURCE_INSERT_STATUS";
+    public static final PerfEventType CONTEXTRESOURCE_INSERTSTATUS = 
+        new PerfEventType(CONTEXTRESOURCE_INSERTSTATUS_TAG, 
+                PerfEventTypeConfig.getInstance().getPerfDelayProperty(CONTEXTRESOURCE_INSERTSTATUS_TAG));
     
+    private static final String CONTEXTRESOURCE_OBSERVERUPDATE_TAG = "CONTEXTRESOURCE_OBSERVER_UPDATE";
+    public static final PerfEventType CONTEXTRESOURCE_OBSERVERUPDATE = 
+        new PerfEventType(CONTEXTRESOURCE_OBSERVERUPDATE_TAG, 
+                PerfEventTypeConfig.getInstance().getPerfDelayProperty(CONTEXTRESOURCE_OBSERVERUPDATE_TAG));
+    
+    
+    private static final String PAGE_ISACCESSIBLE_TAG = "PAGE_IS_ACCESSIBLE";
+    public static final PerfEventType PAGE_ISACCESSIBLE = 
+        new PerfEventType(PAGE_ISACCESSIBLE_TAG, 
+                PerfEventTypeConfig.getInstance().getPerfDelayProperty(PAGE_ISACCESSIBLE_TAG));
+    
+    private static final String PAGE_NEEDSDATA_TAG = "PAGE_NEEDS_DATA";
+    public static final PerfEventType PAGE_NEEDSDATA = 
+        new PerfEventType(PAGE_NEEDSDATA_TAG, 
+                PerfEventTypeConfig.getInstance().getPerfDelayProperty(PAGE_NEEDSDATA_TAG));
+    
+    private static final String PAGE_HANDLESUBMITTEDDATA_TAG = "PAGE_HANDLE_SUBMITTED_DATA";
+    public static final PerfEventType PAGE_HANDLESUBMITTEDDATA = 
+        new PerfEventType(PAGE_HANDLESUBMITTEDDATA_TAG,
+                PerfEventTypeConfig.getInstance().getPerfDelayProperty(PAGE_HANDLESUBMITTEDDATA_TAG));
+    
+    private static final String PAGE_INITIWRAPPERS_TAG = "PAGE_INIT_IWRAPPERS";
+    public static final PerfEventType PAGE_INITIWRAPPERS = 
+        new PerfEventType(PAGE_INITIWRAPPERS_TAG, 
+                PerfEventTypeConfig.getInstance().getPerfDelayProperty(PAGE_INITIWRAPPERS_TAG));
+    
+    private static final String PAGE_RETRIEVECURRENTSTATUS_TAG = "PAGE_RETRIEVE_CURRENT_STATUS"; 
+    public static final PerfEventType PAGE_RETRIEVECURRENTSTATUS = 
+        new PerfEventType(PAGE_RETRIEVECURRENTSTATUS_TAG, 
+                PerfEventTypeConfig.getInstance().getPerfDelayProperty(PAGE_RETRIEVECURRENTSTATUS_TAG));
+    
+    
+    private static final String CONTEXT_CREATENAVICOMPLETE_TAG = "CONTEXT_CREATE_NAVI_COMPLETE";
+    public static final PerfEventType CONTEXT_CREATENAVICOMPLETE = 
+        new PerfEventType(CONTEXT_CREATENAVICOMPLETE_TAG, 
+                PerfEventTypeConfig.getInstance().getPerfDelayProperty(CONTEXT_CREATENAVICOMPLETE_TAG));
+    
+    private static final String CONTEXT_CREATENAVIREUSE_TAG = "CONTEXT_CREATE_NAVI_REUSE";
+    public static final PerfEventType CONTEXT_CREATENAVIREUSE = 
+        new PerfEventType(CONTEXT_CREATENAVIREUSE_TAG,
+                PerfEventTypeConfig.getInstance().getPerfDelayProperty(CONTEXT_CREATENAVIREUSE_TAG));
+    
+    
+    private static final String XMLSERVER_HANDLEDOCUMENT_TAG = "XMLSERVER_HANDLE_DOCUMENT";
+    public static final PerfEventType XMLSERVER_HANDLEDOCUMENT = 
+        new PerfEventType(XMLSERVER_HANDLEDOCUMENT_TAG, 
+                PerfEventTypeConfig.getInstance().getPerfDelayProperty(XMLSERVER_HANDLEDOCUMENT_TAG));
+    
+    private static final String XMLSERVER_GETDOM_TAG = "XMLSERVER_GET_DOM";
+    public static final PerfEventType XMLSERVER_GETDOM = 
+        new PerfEventType(XMLSERVER_GETDOM_TAG, 
+                PerfEventTypeConfig.getInstance().getPerfDelayProperty(XMLSERVER_GETDOM_TAG));
+    
+    private static final String XMLSERVER_CALLPROCESS_TAG = "XMLSERVER_CALL_PROCESS";
+    public static final PerfEventType XMLSERVER_CALLPROCESS = 
+        new PerfEventType(XMLSERVER_CALLPROCESS_TAG, 
+                PerfEventTypeConfig.getInstance().getPerfDelayProperty(XMLSERVER_CALLPROCESS_TAG));
+    
+    private static final String XMLSERVER_PREPROCESS_TAG = "XMLSERVER_PRE_PROCESS";
+    public static final PerfEventType XMLSERVER_PREPROCESS = 
+        new PerfEventType(XMLSERVER_PREPROCESS_TAG, 
+                PerfEventTypeConfig.getInstance().getPerfDelayProperty(XMLSERVER_PREPROCESS_TAG));
     
     private String tag;
     private long delay;
