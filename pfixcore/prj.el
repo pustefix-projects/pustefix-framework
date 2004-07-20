0af69aa821fe70c4a0ecf5a145292b2f17a51a45
(noauthor) <noreply@pustefixframework.org>
2004-07-20 11:56:26 +0000

    This commit was manufactured by cvs2svn to create branch 'editor2'.

--- pfixcore/prj.el
+++
@@ -1,8 +0,0 @@
-(jde-project-file-version "1.0")
-(jde-set-variables
- '(jde-make-working-directory "./")
- '(jde-global-classpath (quote ("./src" "./build" "./res" "./lib" "./example/servletconf/tomcat/common/lib/servlet.jar")))
- '(jde-run-working-directory "./")
- '(jde-compile-option-directory "./build")
- '(jde-sourcepath (quote ("./src")))
- '(jde-built-class-path (quote ("./build"))))
--- pfixcore/src/de/schlund/util/FactoryInit.java
+++
@@ -1,7 +0,0 @@
-package de.schlund.util;
-
-import java.util.*;
-
-public interface FactoryInit {
-    public void init(Properties props) throws Exception;
-}
