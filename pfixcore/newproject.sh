b331c7c05b5fcce60029ce5545fc419a3accf344
(noauthor) <noreply@pustefixframework.org>
2004-08-06 15:02:52 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.2.8'.

--- pfixcore/newproject.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-mylogjar=`ls lib/log*.jar;`
-
-java -Dnewprjprops="$PWD/example/common/conf/newproject.prop" -classpath $mylogjar:build de.schlund.pfixcore.util.basicapp.basics.InitNewPfixProject
--- pfixcore/skel/newproject.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-mylogjar=`ls lib/log*.jar;`
-mycorejar=`ls lib/pfixcore*.jar`
-
-java -Dnewprjprops="$PWD/projects/common/conf/newproject.prop" -classpath $mylogjar:$mycorejar de.schlund.pfixcore.util.basicapp.basics.InitNewPfixProject
