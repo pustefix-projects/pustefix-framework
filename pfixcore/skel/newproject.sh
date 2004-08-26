68f021c89de922d66175a557022e7476aae08b5b
mlhartme <noreply@pustefixframework.org>
2004-08-26 09:16:23 +0000

    add missing jar files

--- pfixcore/skel/newproject.sh
+++ pfixcore/skel/newproject.sh
@@ -2,5 +2,8 @@
 
 mylogjar=`ls lib/log*.jar;`
 mycorejar=`ls lib/pfixcore*.jar`
+myxmljar=`ls lib/xml*.jar;`
+myxercesjar=`ls lib/xerces*.jar;`
+mysaxonjar=`ls lib/saxon*.jar;`
 
-java -Dnewprjprops="$PWD/projects/common/conf/newproject.prop" -classpath $mylogjar:$mycorejar de.schlund.pfixcore.util.basicapp.basics.InitNewPfixProject
+java -Dnewprjprops="$PWD/projects/common/conf/newproject.prop" -classpath $mylogjar:$mysaxonjar:$myxmljar:$myxercesjar:$mycorejar de.schlund.pfixcore.util.basicapp.basics.InitNewPfixProject
