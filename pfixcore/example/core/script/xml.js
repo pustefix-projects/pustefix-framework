4359a782908c1e92d033a8b39f1c68bdd38bfd73
ecker <noreply@pustefixframework.org>
2004-09-03 16:46:54 +0000

    XMLHttpRequest and ActiveX combined

--- pfixcore/example/core/script/xml.js
+++ pfixcore/example/core/script/xml.js
@@ -6,9 +6,8 @@ var _xml = [];
 var _xmlThis = [];
 var _xmlTimer = [];
 var _xmlTimerCount = [];
-var _xmlTimerCountMax = 100;
-var _xmlTimerInterval = 100;
-var _msXmlHttp;
+var _xmlTimerCountMax = 1000;
+var _xmlTimerInterval = 5;
 
 //*****************************************************************************
 //
@@ -20,9 +19,18 @@ function xmlRequest() {
   this.callback = arguments[2];
   this.context  = arguments[3];
 
-  this.timer = [];
-  this.timerCount = [];
+  this.iframes  = 1;
+
+  this.msXmlHttp = "";
+
+  this.headers = [ [ 'SOAPAction', '""'] ];
+  this.errors = [];
+  this.status = 0;
 }
+
+xmlRequest.prototype.IFRAMES_NEVER    = -1;
+xmlRequest.prototype.IFRAMES_FALLBACK =  0;
+xmlRequest.prototype.IFRAMES_ONLY     =  1;
 
 //*****************************************************************************
 //
@@ -33,165 +41,224 @@ xmlRequest.prototype.start = function( content ) {
   var uniq = ""+ new Date().getTime() + Math.floor(1000 * Math.random());
   //  this.url += ( ( this.url.indexOf('?')+1 ) ? '&' : '?' ) + uniq;
 
-  if( this.callback ) {
-    //        alert(this.callback.valueOf());
-  }
-
   var i = _xml.length;
 
-  if( 0 && window.XMLHttpRequest ) {
+  if( this.iframes!=this.IFRAMES_ONLY ) {
 
-    //----------------
-    // XMLHttpRequest
-    //----------------
+    if( window.XMLHttpRequest ) {
 
-    try {
-      _xml[i] = new XMLHttpRequest();
+      //----------------
+      // XMLHttpRequest
+      //----------------
 
-      if( this.callback ) {
-        var self = this;
-        _xml[i].onreadystatechange = function() {
-          if( _xml[i].readyState == 4 ) { 
-            if( _xml[i].status < 300 ) { 
-              self.callback.call( self.context, _xml[i].responseXML); 
-            } else { 
-              throw "Asynchronous call failed"; 
-            }
-          }
-        };
+      try {
+        _xml[i] = new XMLHttpRequest();
+        
+      } catch(e) {
+        _xml[i] = null;
+        if( this.iframes == this.IFRAMES_NEVER ) {
+          throw "xmlRequest: Could not create XMLHttpRequest";
+        } 
       }
-      _xml[i].open( this.method, this.url, this.callback ? true : false);
-      _xml[i].setRequestHeader("SOAPAction", '""');
-      _xml[i].send(content);
+    } else if( window.ActiveXObject ) {
 
-      if( !this.callback ) {
-        return _xml[i].responseXML;
-      } else {
-        return true;
+      //---------
+      // ActiveX
+      //---------
+
+      if( !this.msXmlHttp ) {
+        // determine working XMLHTTP component
+        var msXmlHttpList = ["MSXML2.XMLHTTP.5.0", "MSXML2.XMLHTTP.4.0", "MSXML2.XMLHTTP.3.0", "MSXML2.XMLHTTP", "MICROSOFT.XMLHTTP.1.0", "MICROSOFT.XMLHTTP.1", "MICROSOFT.XMLHTTP"];
+        
+        for (var j=0; j<msXmlHttpList.length; j++) {
+          try {
+            _xml[i] = new ActiveXObject(msXmlHttpList[j]);
+            
+            // success ==> store, quit loop
+            this.msXmlHttp = msXmlHttpList[j];
+            break;
+          } catch(e) {
+            // ignore failures
+          } 
+        }
       }
-    } catch(e) {
-      _xml[i] = null;
-      throw "Exception:" + e;
-    }    
-	}
 
-  if( 0 && !_xml[i] && window.ActiveXObject ) {
-
-    //---------
-    // ActiveX
-    //---------
-
-    if( !_msXmlHttp ) {
-      // determine working XMLHTTP component
-      var msXmlHttpList = ["MSXML2.XMLHTTP.5.0", "MSXML2.XMLHTTP.4.0", "MSXML2.XMLHTTP.3.0", "MSXML2.XMLHTTP", "MICROSOFT.XMLHTTP.1.0", "MICROSOFT.XMLHTTP.1", "MICROSOFT.XMLHTTP"];
-      
-      for (var j=0; j<msXmlHttpList.length; j++) {
-        try {
-          _xml[i] = new ActiveXObject(msXmlHttpList[j]);
-          
-          // success ==> store, quit loop
-          _msXmlHttp = msXmlHttpList[j];
-          break;
-        } catch(e) {
-          // ignore failures
+      try {
+        _xml[i] = new ActiveXObject(this.msXmlHttp);
+      } catch(e) {
+        _xml[i] = null;
+        if( this.iframes == this.IFRAMES_NEVER ) {
+          throw "xmlRequest: Could not create ActiveXObject " + this.msXmlHttp;
         } 
       }
     }
-    alert("_msXmlHttp:" + _msXmlHttp);
+    
+    if( _xml[i] ) {
 
-    try {
-      _xml[i] = new ActiveXObject(_msXmlHttp);
-      _xml[i].onreadystatechange = new Function( 'if( _xml['+i+'].readyState == 4 ) { '+this.callback+'(_xml['+i+']); }' );
-      _xml[i].open( this.method, this.url, this.callback ? true : false);
-      _xml[i].setRequestHeader("SOAPAction", '""');
-      _xml[i].send(content);
+      if( this.callback ) {
+        //-------
+        // async
+        //-------
 
-      if( !this.callback ) {
-        return _xml[i].responseXML;
-      } else {
-        return true;
+        try {
+          var self = this;
+
+          if( this.msXmlHttp ) {
+            _xml[i].onreadystatechange = function() {
+              if( _xml[i].readyState == 4 ) {
+                self.callback.call( self.context, _xml[i].responseXML); 
+              }
+            };
+          } else {
+            _xml[i].onreadystatechange = function() {
+              if( _xml[i].readyState == 4 ) {
+                self.status = _xml[i].status;
+                if( _xml[i].status < 300 ) { 
+                  self.callback.call( self.context, _xml[i].responseXML); 
+                } else {
+                  throw "xmlRequest: Asynchronous call failed" + " (status " + _xml[i].status + ")";
+                }
+              }
+            };
+          }
+
+        } catch(e) {
+          _xml[i] = null;
+          throw "xmlRequest: onreadystatechange failed";
+        }
       }
-    } catch(e) {
-      _xml[i] = null;
-      alert("Exception1:" + e.message);
+
+      try {
+        _xml[i].open( this.method, this.url, this.callback ? true : false); 
+          
+        for( var j=0; j<this.headers.length; j++ ) {
+          _xml[i].setRequestHeader( this.headers[j][0], this.headers[j][1] );
+        }
+          
+        _xml[i].send(content);
+          
+        if( !this.callback ) {
+          return _xml[i].responseXML;
+        } else {
+          return true;
+        }
+      } catch(e) {
+        _xml[i] = null;
+        throw "xmlRequest: call failed";
+      }  
     }
   }
 
-  if( !_xml[i] && document.createElement && document.childNodes ) {
+  if( this.iframes!=this.IFRAMES_NEVER && !_xml[i] && document.createElement && document.childNodes ) {
 
     //--------
     // iframe
     //--------
 
-    var i = _xml.length;
+    try {
 
-    var iframe = document.createElement("iframe");
-		iframe.style.visibility = "visible";
-    //     iframe.style.position   = "absolute";
-    //     iframe.style.left       = "0px";
-    //     iframe.style.top        = "0px";
-    iframe.style.width      = "600px";
-    iframe.style.height     = "400px";    
-    iframe.id               = "pfxxmliframe"+i;
-    iframe.name             = "pfxxmliframe"+i;
+      var iframe;
 
-    iframe.src              = this.url;
-    if( this.method.toUpperCase() == "POST" ) {
-      // load dummy page to prevent cross-domain security error
-      iframe.src = iframe.src.replace(/(https?:\/\/[^\/]+)(.*)/, "$1/blank.html");
-    }
+      //--------------
+      // result frame
+      //--------------
 
-    document.body.appendChild(iframe);
+      iframe = document.createElement("iframe");
+      iframe.style.visibility = "hidden";
+      //     iframe.style.position   = "absolute";
+      //     iframe.style.left       = "0px";
+      //     iframe.style.top        = "0px";
+      iframe.style.width      = "0px";
+      iframe.style.height     = "0px";    
+      iframe.id               = "pfxxmliframe"+i;
+      iframe.name             = "pfxxmliframe"+i;
 
-    _xml[i] = this.callback;
-    _xmlThis[i] = this;
-    _xmlTimer[i] = true;
-    _xmlTimerCount[i] = 0;
+      if( this.method.toUpperCase() == "GET" ) {
 
-    var self = this;
-    if( this.method.toUpperCase() == "POST" ) {
-      setTimeout( function() {
+        //-----
+        // GET
+        //-----
 
-        var iDoc;
-        try {
-          iDoc =  iframe.contentDocument;
-        } catch(e) {
-          try {
-            iDoc = contentWindow.document;
-          } catch(e) {
-            throw "could not use iframe";
-          }
-        }
-
-        //        alert("iDoc:" + iDoc);
-        var attr;
-        var form = iDoc.createElement("form");
-                //         attr = iDoc.createAttribute("action");
-        //         attr.nodeValue = self.url;
-        //         form.setAttributeNode(attr);
-        form.action = self.url;
-        form.method = "POST";
-
-        var field = iDoc.createElement("textarea");
-        field.name = "soapmessage";
-        field.value = content;
-        form.appendChild(field);
-
-        iDoc.body.appendChild(form);
-        
-        iDoc.forms[iDoc.forms.length-1].submit();
-
+        iframe.src            = this.url;
+        document.body.appendChild(iframe);
         _xmlTimer[i] = window.setInterval('customOnReadyStateChange()', _xmlTimerInterval);
-      }, 50 );
-    } else {
-      _xmlTimer[i] = window.setInterval('customOnReadyStateChange()', _xmlTimerInterval);
-    }
 
-    return "iframe";
+      } else {
+
+        //-------------------------------------------
+        // POST (or others, which are not supported)
+        //-------------------------------------------
+
+        document.body.appendChild(iframe);
+
+        //------------
+        // form frame
+        //------------
+
+        iframe = document.createElement("iframe");
+        iframe.style.visibility = "hidden";
+        //     iframe.style.position   = "absolute";
+        //     iframe.style.left       = "0px";
+        //     iframe.style.top        = "0px";
+        iframe.style.width      = "0px";
+        iframe.style.height     = "0px";    
+        iframe.id               = "pfxxmliframeform"+i;
+        iframe.name             = "pfxxmliframeform"+i;
+
+        document.body.appendChild(iframe);
+
+        _xml[i] = this.callback;
+        _xmlThis[i] = this;
+        _xmlTimer[i] = true;
+        _xmlTimerCount[i] = 0;
+
+        var self = this;
+        setTimeout( function() {
+
+          var iDoc;
+          try {
+            iDoc = iframe.contentWindow.document;
+          } catch(e) {
+            try {
+              iDoc =  iframe.contentDocument;
+            } catch(e) {
+              throw "could not use iframe";
+            }
+          }
+
+          var form = iDoc.createElement("form");
+          form.action = self.url;
+          form.target = "pfxxmliframe"+i;
+          form.method = self.method;
+
+          var field = iDoc.createElement("textarea");
+          field.name = "soapmessage";
+          field.value = content;
+          form.appendChild(field);
+
+          iDoc.body.appendChild(form);
+        
+          iDoc.forms[iDoc.forms.length-1].submit();
+
+          _xmlTimer[i] = window.setInterval('customOnReadyStateChange()', _xmlTimerInterval);
+        }, 5 );
+      }
+
+      return "iframe";
+    } catch(e) {
+      throw "xmlRequest: Iframes failed";
+    }
   }
 
-  // Error
-  return false;
+  throw "xmlRequest: failed";
+};
+
+//*****************************************************************************
+//
+//*****************************************************************************
+xmlRequest.prototype.setRequestHeader = function( field, value ) {
+  
+  this.headers.push( [field, value] );
 };
 
 //*****************************************************************************
@@ -202,12 +269,8 @@ function customOnReadyStateChange() {
   for( var i=0; i<_xml.length; i++ ) {
     if( _xmlTimer[i] && _xml[i] ) {
 
-      //      document.getElementById("dbg").value += "\n=" + _xmlTimerCount[i] + ", ";
-
       try {
         if( _xmlTimerCount[i]<_xmlTimerCountMax ) {
-
-          //          alert("XXX:" + window.frames['pfxxmliframe'+i].location);
 
           if( window.frames['pfxxmliframe'+i] && window.frames['pfxxmliframe'+i].document && window.frames['pfxxmliframe'+i].document.body && !window.frames['pfxxmliframe'+i].document.body.innerHTML && 
               (/blank\.hmtl$/.test(window.frames['pfxxmliframe'+i].location) || /about\:blank$/.test(window.frames['pfxxmliframe'+i].location)) ) {
@@ -235,8 +298,7 @@ function cancelOnReadyStateChange( i, msg ) {
 
   try {
     if( msg ) {
-      alert("cancelOnReadyStateChange:" + i + ", " + msg);
-      //      document.getElementById("dbg").value += "\n=" + i + " " + msg;
+      //      alert("cancelOnReadyStateChange:" + i + ", " + msg);
     }
     window.clearInterval(_xmlTimer[i]);
     _xmlTimer[i] = null;
@@ -245,29 +307,15 @@ function cancelOnReadyStateChange( i, msg ) {
   }
 
   try {
-    //    alert(document.getElementById("pfxxmliframe"+i));
-    //    alert(document.body.childNodes.length);
-    //    for( var j=0; j<document.body.childNodes.length; j++ ) {
-    //      alert( document.body.childNodes[j] );
-    //    }
-
-//    document.body.removeChild(document.getElementById("pfxxmliframe"+i));
+    var iframe;
+    if( iframe = document.getElementById("pfxxmliframe"+i) ) {
+      document.body.removeChild( iframe );
+    }
+    if( iframe = document.getElementById("pfxxmliframeform"+i) ) {
+      document.body.removeChild( iframe );
+    }
   } catch(e) {
     alert("Exception3:" + e);
-  }
-}
-
-//*****************************************************************************
-//
-//*****************************************************************************
-function sleepMSec( msec, i ) {
-
-  return;
-
-  var t0 = (new Date()).getTime();
-  var t1;
-
-  while( (new Date().getTime()-t0<msec) ) {
   }
 }
 //*****************************************************************************
