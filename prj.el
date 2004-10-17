70c323d94d7d06998288fbf8ba48a2c9a1dd8f80
jenstl <noreply@pustefixframework.org>
2004-10-17 00:12:41 +0000

    use correct servlet-apt.jar in classpath

--- prj.el
+++ prj.el
@@ -1,7 +1,7 @@
 (jde-project-file-version "1.0")
 (jde-set-variables
  '(jde-make-working-directory "./")
- '(jde-global-classpath (quote ("./src" "./build" "./res" "./lib" "./example/servletconf/tomcat/common/lib/servlet.jar" "./target/test-classes")))
+ '(jde-global-classpath (quote ("./src" "./build" "./res" "./lib" "./example/servletconf/tomcat/common/lib/servlet-api.jar" "./target/test-classes")))
  '(jde-run-working-directory "./")
  '(jde-compile-option-directory "./build")
  '(jde-sourcepath (quote ("./src" "./tests/junit")))
