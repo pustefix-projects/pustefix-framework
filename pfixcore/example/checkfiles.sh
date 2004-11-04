c2ac3fad453a633a7dad8c46357d5ba1c0711830
jenstl <noreply@pustefixframework.org>
2004-11-04 23:14:24 +0000

    *** empty log message ***

--- pfixcore/example/checkfiles.sh
+++ pfixcore/example/checkfiles.sh
@@ -7,4 +7,7 @@ find . -type f | grep "/txt/" | grep "\.xml$"  > .ALLINCFILES
 find . -type f | grep "/img/" | grep "\.\(gif\|jpg\|jpeg\|JPG\|JPEG\)$"  > .ALLIMAGES
 find . -type f | grep "depend.xml$"  > .ALLPROJECTS
 
-java de.schlund.pfixcore.util.CheckIncludes CHECKOUTPUT.xml .ALLPROJECTS .ALLINCFILES .ALLIMAGES
+java -mx500M de.schlund.pfixcore.util.CheckIncludes CHECKOUTPUT.xml .ALLPROJECTS .ALLINCFILES .ALLIMAGES
+java -mx500M com.icl.saxon.StyleSheet  CHECKOUTPUT.xml core/build/unused.xsl  > UnusedInfo.txt
+java -mx500M com.icl.saxon.StyleSheet  CHECKOUTPUT.xml core/build/cleanup.xsl > Cleanup.xml
+
