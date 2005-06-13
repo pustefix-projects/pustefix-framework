882e24e109b83f613d582b036599cf1bf2fca614
haexel <noreply@pustefixframework.org>
2005-06-13 18:38:32 +0000

    *** empty log message ***

---
+++ pfixcore/src/de/schlund/pfixxml/perflogging/Interval.java
@@ -0,0 +1,54 @@
+/*
+ * Created on 31.05.2005
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package de.schlund.pfixxml.perflogging;
+
+/**
+ * @author jh
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+class Interval {
+    private long from;
+    private long until;
+    
+    Interval(long from, long until) {
+        this.from = from;
+        this.until = until;
+    }
+    
+    boolean contains(long value) {
+        boolean ret = (value >= from && value < until);
+        //System.out.println("Contains: "+value+":"+ret+"-->"+toString());
+        return ret;
+    }
+    
+    boolean isGreater(long value) {
+        boolean ret = value >= until;
+        //System.out.println("greater: "+value+":"+ret+"-->"+toString());
+        return ret;
+    }
+    
+    boolean isLess(long value) {
+        boolean ret = value < from;
+        //System.out.println("less: "+value+":"+ret+"-->"+toString());
+        return ret;
+    }
+    
+    long getFrom() {
+        return from;
+    }
+    
+    long getUntil() {
+        return until;
+    }
+    
+    public String toString() {
+        return "["+from+"-"+until+"]";
+    }
+
+}
