c8980e91dd44cfbe640a8ba5dbb765ce585765a0
(noauthor) <noreply@pustefixframework.org>
2005-07-11 11:00:49 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.37.4'.

--- pfixcore/prj.el
+++
@@ -1,8 +0,0 @@
-(jde-project-file-version "1.0")
-(jde-set-variables
- '(jde-make-working-directory "./")
- '(jde-global-classpath (quote ("./build" "./res" "./lib" "./example/servletconf/tomcat/common/lib/servlet-api.jar" "./target/test-classes")))
- '(jde-run-working-directory "./")
- '(jde-compile-option-directory "./build")
- '(jde-sourcepath (quote ("./src" "./tests/junit" "${JAVA_HOME}/src.zip")))
- '(jde-built-class-path (quote ("./build"))))
