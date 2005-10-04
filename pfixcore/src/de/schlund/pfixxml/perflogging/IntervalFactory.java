dfb50caf995dc7a0f6c908ba37f007a2eb98c04d
haexel <noreply@pustefixframework.org>
2005-10-04 16:07:15 +0000

    ready to be used with jmx

--- pfixcore/src/de/schlund/pfixxml/perflogging/IntervalFactory.java
+++ pfixcore/src/de/schlund/pfixxml/perflogging/IntervalFactory.java
@@ -20,11 +20,11 @@ public class IntervalFactory {
     private static IntervalFactory instance = new IntervalFactory();
     private int interval_size = 20;
     private int increase_factor = 2;
-    private HashMap interval_category_map;
+    private HashMap<String, List<Interval> > interval_category_map;
     
     
     private IntervalFactory() {
-        interval_category_map = new HashMap();
+        interval_category_map = new HashMap<String, List<Interval>>();
     }
     
     
@@ -33,31 +33,31 @@ public class IntervalFactory {
         return instance;
     }
     
-    List getIntervalForCategory(String category) {
+    List<Interval> getIntervalForCategory(String category) {
         if(!interval_category_map.containsKey(category)) {
-            List intervals = createInterval();
+            List<Interval> intervals = createInterval();
             interval_category_map.put(category, intervals);
         }
-        return (List) interval_category_map.get(category);
+        return interval_category_map.get(category);
     }
     
     HashMap getAllIntervals() {
         return interval_category_map;
     }
     
-    private List createInterval() {
-        List intervals = new ArrayList(interval_size); 
+    private List<Interval> createInterval() {
+        List<Interval> intervals = new ArrayList<Interval>(interval_size); 
         intervals.add(0, new Interval(0, 1));
         intervals.add(1, new Interval(1, increase_factor));
         
         for(int i=2; i<interval_size -1; i++) {
-            long pre_until = ((Interval) intervals.get(i-1)).getUntil(); 
+            long pre_until = (intervals.get(i-1)).getUntil(); 
             intervals.add(i,new Interval(
                      pre_until, pre_until * increase_factor)); 
         }
         
         intervals.add(interval_size -1, new Interval(
-                ((Interval) intervals.get(interval_size -2)).getUntil(), Long.MAX_VALUE));
+                (intervals.get(interval_size -2)).getUntil(), Long.MAX_VALUE));
         
        /* for(int i=0; i<interval_size; i++) {
             System.out.print(i+":->"+intervals.get(i)+"|");
