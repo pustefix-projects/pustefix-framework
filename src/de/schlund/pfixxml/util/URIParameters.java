df4b14f6079a4a4edfa98c72403ef2f03c894008
jenstl <noreply@pustefixframework.org>
2006-01-27 17:10:50 +0000

    *** empty log message ***

--- src/de/schlund/pfixxml/util/URIParameters.java
+++ src/de/schlund/pfixxml/util/URIParameters.java
@@ -25,79 +25,79 @@ import org.apache.log4j.Category;
  */
 public class URIParameters {
     
-	private Category CAT=Category.getInstance(getClass().getName());
-	private boolean DEBUG=CAT.isDebugEnabled();
+    private Category CAT=Category.getInstance(getClass().getName());
+    private boolean DEBUG=CAT.isDebugEnabled();
     
-	HttpServletRequest request;
-	String encoding;
-   HashMap params;
+    HttpServletRequest request;
+    String encoding;
+    HashMap params;
     
-   public URIParameters(HttpServletRequest request,String encoding) throws Exception {
+    public URIParameters(HttpServletRequest request,String encoding) throws Exception {
    	this.request=request;
    	this.encoding=encoding;
-      params=parse(request.getQueryString(),encoding);
-      if(DEBUG) CAT.debug(toString());
-   }
-
-   public HttpServletRequest getRequest() {
+        params=parse(request.getQueryString(),encoding);
+        if(DEBUG) CAT.debug(toString());
+    }
+    
+    public HttpServletRequest getRequest() {
    	return request;
-   }
-   
-   public String getEncoding() {
+    }
+    
+    public String getEncoding() {
    	return encoding;
-   }
-   
-   private HashMap parse(String queryStr,String encoding) throws Exception {
+    }
+    
+    private HashMap parse(String queryStr,String encoding) throws Exception {
    	HashMap map=new HashMap();
-      if(queryStr!=null) {
-      	StringTokenizer st=new StringTokenizer(queryStr,"&");
-      	while(st.hasMoreTokens()) {
+        if(queryStr!=null) {
+            StringTokenizer st=new StringTokenizer(queryStr,"&");
+            while(st.hasMoreTokens()) {
       		String pair=st.nextToken();
       		int ind=pair.indexOf('=');
       		if(ind>0) {
-      			String key=pair.substring(0,ind);
-      			String val=pair.substring(ind+1);
-      			String decKey=URLDecoder.decode(key,encoding);
-      			String decVal=URLDecoder.decode(val,encoding);
-      			String[] vals=(String[])map.get(decKey);
-      			if(vals==null) vals=new String[] {decVal};
-      			else {
-      				String[] tmp=new String[vals.length+1];
-      				for(int i=0;i<vals.length;i++) tmp[i]=vals[i];
-      				tmp[vals.length]=decVal;
-      				vals=tmp;
-      			}
-      			map.put(decKey,vals); 
+                    String key=pair.substring(0,ind);
+                    String val=pair.substring(ind+1);
+                    String decKey=URLDecoder.decode(key,encoding);
+                    String decVal=URLDecoder.decode(val,encoding);
+                    String[] vals=(String[])map.get(decKey);
+                    if(vals==null) vals=new String[] {decVal};
+                    else {
+                        String[] tmp=new String[vals.length+1];
+                        for(int i=0;i<vals.length;i++) tmp[i]=vals[i];
+                        tmp[vals.length]=decVal;
+                        vals=tmp;
+                    }
+                    map.put(decKey,vals); 
       		}
-      	}
-      }
-      return map;
-   }
+            }
+        }
+        return map;
+    }
     
-   public String getParameter(String name) {
+    public String getParameter(String name) {
    	String[] vals=(String[])params.get(name);
    	if(vals!=null && vals.length>0) return vals[0];
    	return null;
-   }
+    }
     
-   public Enumeration getParameterNames() {
+    public Enumeration getParameterNames() {
    	Vector v=new Vector();
    	if(!params.isEmpty()) {
-   		Iterator it=params.keySet().iterator();
-   		while(it.hasNext()) v.add(it.next());
+            Iterator it=params.keySet().iterator();
+            while(it.hasNext()) v.add(it.next());
    	}
    	return v.elements();
-   }
+    }
     
-   public String[] getParameterValues(String name) {
+    public String[] getParameterValues(String name) {
    	return (String[])params.get(name);
-   }
-  
-   public Map getParameterMap() {
-   	return params;
-   }
+    }
     
-   public String toString() {
+    public Map getParameterMap() {
+   	return params;
+    }
+    
+    public String toString() {
    	StringBuffer sb=new StringBuffer();
    	sb.append("URIParameters");
    	sb.append(" (");
@@ -105,18 +105,18 @@ public class URIParameters {
    	sb.append("):\n");
    	Iterator it=params.keySet().iterator();
    	while(it.hasNext()) {
-   		String name=(String)it.next();
-   		sb.append("   ");
-   		sb.append(name);
-   		sb.append(" = ");
-   		String[] values=(String[])params.get(name);
-   		for(int i=0;i<values.length;i++) {
-   			sb.append(values[i]);
-            if(i<values.length-1) sb.append(" | ");
-   		}
-   		sb.append("\n");
+            String name=(String)it.next();
+            sb.append("   ");
+            sb.append(name);
+            sb.append(" = ");
+            String[] values=(String[])params.get(name);
+            for(int i=0;i<values.length;i++) {
+                sb.append(values[i]);
+                if(i<values.length-1) sb.append(" | ");
+            }
+            sb.append("\n");
    	}
-      return sb.toString();
-   }
+        return sb.toString();
+    }
     
 }
