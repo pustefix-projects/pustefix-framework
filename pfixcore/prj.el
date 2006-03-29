eafae2dc120a1d6f7e26616bd88cad94d8233667
jenstl <noreply@pustefixframework.org>
2006-03-29 10:23:53 +0000

    big move of example=>projects

--- pfixcore/prj.el
+++ pfixcore/prj.el
@@ -1,6 +1,6 @@
 (jde-project-file-version "1.0")
 (jde-set-variables
- '(jde-global-classpath (quote ("./build" "./res" "./lib" "./example/servletconf/tomcat/common/lib/servlet-api.jar" "./target/test-classes")))
+ '(jde-global-classpath (quote ("./build" "./res" "./lib" "./projects/servletconf/tomcat/common/lib/servlet-api.jar" "./target/test-classes")))
  '(jde-run-working-directory "./")
  '(jde-compile-option-directory "./build")
  '(jde-sourcepath (quote ("./src" "./gensrc" "./tests/junit" "${JAVA_HOME}/src.zip")))
