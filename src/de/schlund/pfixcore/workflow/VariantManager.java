a7fab53ac1dc875016de25536d0674894af234c2
smarsching <noreply@pustefixframework.org>
2006-05-17 17:08:01 +0000

    Fixing bug in VariantManager

--- src/de/schlund/pfixcore/workflow/VariantManager.java
+++ src/de/schlund/pfixcore/workflow/VariantManager.java
@@ -84,7 +84,7 @@ public class VariantManager implements ConfigurableObject {
             String[] variant_arr = variant.getVariantFallbackArray();
             for (int i = 0; i < variant_arr.length; i++) {
                 String   fullname = name + "::" + variant_arr[i];
-                if (contextConfig.getPageFlowConfig(name) != null) {
+                if (contextConfig.getPageFlowConfig(fullname) != null) {
                     LOG.debug("=== Found FLOW for '" + fullname + "' ===");
                     return fullname;
                 }
