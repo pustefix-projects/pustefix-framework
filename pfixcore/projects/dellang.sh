692761cbfc3c8b0a78e04236bf8abfc0587d9033
jenstl <noreply@pustefixframework.org>
2006-09-15 09:38:59 +0000

    removed dellang

--- pfixcore/projects/dellang.sh
+++
@@ -1,7 +0,0 @@
-find . -type f | grep "/txt/" | grep "\.xml$" > ALLINC
-find . -type f | grep "/dyntxt/" |grep "\.xml$" >> ALLINC
-export CLASSPATH=`ls \`pwd\`/../lib/*.jar | xargs echo | sed -e 's/ /:/g'`:`pwd`/../build
-export LANG=C
-
-java -mx384M de.schlund.pfixcore.util.DelLang `pwd` ALLINC
-
