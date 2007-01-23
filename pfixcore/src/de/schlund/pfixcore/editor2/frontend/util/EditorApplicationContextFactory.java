0bee1b01a78f347622188f11538871aaeafc7b97
smarsching <noreply@pustefixframework.org>
2007-01-23 17:28:12 +0000

    Changed editor startup

--- pfixcore/src/de/schlund/pfixcore/editor2/frontend/util/EditorApplicationContextFactory.java
+++ pfixcore/src/de/schlund/pfixcore/editor2/frontend/util/EditorApplicationContextFactory.java
@@ -42,6 +42,10 @@ public final class EditorApplicationContextFactory implements Runnable {
     private Properties initProps;
 
     private boolean initialized = false;
+    
+    private boolean runningInit = false;
+    
+    private boolean initFailed = false;
 
     private Object initLock = new Object();
 
@@ -62,6 +66,7 @@ public final class EditorApplicationContextFactory implements Runnable {
      *             If any error occurs during initialization
      */
     public void init(Properties props) throws Exception {
+        this.runningInit = true;
         this.initProps = props;
         Thread thread = new Thread(this, "editor-init");
         thread.start();
@@ -73,7 +78,7 @@ public final class EditorApplicationContextFactory implements Runnable {
      * @return Spring ApplicationContext
      */
     public ApplicationContext getApplicationContext() {
-        while (!this.initialized) {
+        while (this.runningInit) {
             synchronized (this.initLock) {
                 try {
                     this.initLock.wait();
@@ -82,55 +87,68 @@ public final class EditorApplicationContextFactory implements Runnable {
                 }
             }
         }
-        return this.appContext;
+        if (this.initialized) {
+            return this.appContext;
+        } else if (this.initFailed) {
+            throw new RuntimeException("Editor initialization failed!");
+        } else {
+            throw new RuntimeException("Editor has not been initialized yet or is not active in this environment.");
+        }
     }
 
     public void run() {
-        String configFile = this.initProps.getProperty("de.schlund.pfixcore.editor2.springconfig");
-
-        if (configFile == null) {
-            String err = "Property de.schlund.pfixcore.editor2.springconfig not set!";
-            Logger.getLogger(this.getClass()).fatal(err);
-        }
         try {
-            String docroot = GlobalConfig.getDocroot();
-            if (docroot == null) {
-                String err = "Docroot has to be set! Editor is not supported in WAR mode!";
+            String configFile = this.initProps.getProperty("de.schlund.pfixcore.editor2.springconfig");
+            
+            if (configFile == null) {
+                String err = "Property de.schlund.pfixcore.editor2.springconfig not set!";
                 Logger.getLogger(this.getClass()).fatal(err);
-                throw new IllegalStateException(err);
             }
-            EditorApplicationContext context = 
-                new EditorApplicationContext(configFile, docroot);
-            this.appContext = context;
-            Logger.getLogger(this.getClass()).info("Initialized ApplicationContext for editor");
-        } catch (RuntimeException e) {
-            String err = "Initialization of ApplicationContext for editor failed!";
-            Logger.getLogger(this.getClass()).fatal(err, e);
-        }
+            try {
+                String docroot = GlobalConfig.getDocroot();
+                if (docroot == null) {
+                    String err = "Docroot has to be set! Editor is not supported in WAR mode!";
+                    Logger.getLogger(this.getClass()).fatal(err);
+                    throw new IllegalStateException(err);
+                }
+                EditorApplicationContext context = new EditorApplicationContext(configFile, docroot);
+                this.appContext = context;
+                Logger.getLogger(this.getClass()).info("Initialized ApplicationContext for editor");
+            } catch (RuntimeException e) {
+                String err = "Initialization of ApplicationContext for editor failed!";
+                Logger.getLogger(this.getClass()).fatal(err, e);
+                return;
+            }
 
-        // Active / deactivate background generation of targets
-        String  generatorProp = this.initProps.getProperty("de.schlund.pfixcore.editor2.updatetargets");
-        boolean generatorFlag = false;
+            // Active / deactivate background generation of targets
+            String generatorProp = this.initProps.getProperty("de.schlund.pfixcore.editor2.updatetargets");
+            boolean generatorFlag = false;
 
-        // System.out.println("############ in EACF: generatorProp: " + generatorProp);
-        
-        if (generatorProp == null || generatorProp.equals("1") || generatorProp.equalsIgnoreCase("true")) {
-            generatorFlag = true;
-        }
+            // System.out.println("############ in EACF: generatorProp: " + generatorProp);
 
-        // There should be only one instance of the update service,
-        // but you can never be sure enough.
-        // Note that we look for the concrete implementation, not the
-        // interface.
-        String beanNames[] = this.appContext.getBeanDefinitionNames(PustefixTargetUpdateServiceImpl.class);
-        for (int i = 0; i < beanNames.length; i++) {
-            // System.out.println("############ in EACF: calling setEnabled: " + generatorFlag);
-            ((PustefixTargetUpdateServiceImpl) this.appContext.getBean(beanNames[i])).enableAutoUpdating(generatorFlag);
-        }
+            if (generatorProp == null || generatorProp.equals("1") || generatorProp.equalsIgnoreCase("true")) {
+                generatorFlag = true;
+            }
 
-        this.initialized = true;
-        synchronized (this.initLock) {
-            this.initLock.notifyAll();
+            // There should be only one instance of the update service,
+            // but you can never be sure enough.
+            // Note that we look for the concrete implementation, not the
+            // interface.
+            String beanNames[] = this.appContext.getBeanDefinitionNames(PustefixTargetUpdateServiceImpl.class);
+            for (int i = 0; i < beanNames.length; i++) {
+                // System.out.println("############ in EACF: calling setEnabled: " + generatorFlag);
+                ((PustefixTargetUpdateServiceImpl) this.appContext.getBean(beanNames[i])).enableAutoUpdating(generatorFlag);
+            }
+
+            this.initialized = true;
+        } finally {
+            if (!this.initialized) {
+                this.initFailed = false;
+            }
+            this.runningInit = false;
+            synchronized (this.initLock) {
+                this.initLock.notifyAll();
+            }
         }
     }
 }
