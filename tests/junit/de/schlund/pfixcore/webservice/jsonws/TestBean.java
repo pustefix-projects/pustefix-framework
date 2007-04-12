b3ab59d6c62f3db17af0620320e96d1a3c35f075
mtld <noreply@pustefixframework.org>
2007-04-12 12:59:52 +0000

    JSON tests

--- tests/junit/de/schlund/pfixcore/webservice/jsonws/TestBean.java
+++ tests/junit/de/schlund/pfixcore/webservice/jsonws/TestBean.java
@@ -1,14 +1,30 @@
 /*
- * Created on 04.04.2007
+ * This file is part of PFIXCORE.
  *
- * To change the template for this generated file go to
- * Window - Preferences - Java - Code Generation - Code and Comments
+ * PFIXCORE is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * PFIXCORE is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with PFIXCORE; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
  */
 package de.schlund.pfixcore.webservice.jsonws;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * @author mleidig@schlund.de
+ */
 public class TestBean {
 
     private int value;
@@ -17,6 +33,7 @@ public class TestBean {
     private List<List<Integer>> intList;
     private Map<String,String> strMap;
     private Map<String,Map<String,Integer>> mapMap;
+    private HashMap<String,Integer> intMap;
     
     public int getValue() {
         return value;
@@ -64,6 +81,14 @@ public class TestBean {
     
     public void setMapMap(Map<String,Map<String,Integer>> mapMap) {
         this.mapMap=mapMap;
+    }
+    
+    public HashMap<String,Integer> getIntMap() {
+        return intMap;
+    }
+    
+    public void setIntMap(HashMap<String,Integer> intMap) {
+        this.intMap=intMap;
     }
     
 }
