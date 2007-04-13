75bed36c12487808cad8256ebe940db56aa79614
mtld <noreply@pustefixframework.org>
2007-04-13 10:24:45 +0000

    fix nexproject.sh

--- pfixcore/skel/newproject.sh
+++ pfixcore/skel/newproject.sh
@@ -3,7 +3,7 @@
 docroot=$PWD/projects/
 
 mylogjar=`ls lib/log*.jar;`
-mysaxonjar=`ls lib/saxon*.jar;`
+mysaxonjar=`ls lib/saxon6*.jar;`
 mypfixcore=`ls lib/pfixcore*.jar;`
 
 java -Dpustefix.docroot=${docroot} -classpath $mypfixcore:$mylogjar:$mysaxonjar:build de.schlund.pfixcore.util.basicapp.basics.InitNewPfixProject
