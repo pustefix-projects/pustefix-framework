596612f4250cdf2797614cd5ae0127ff91df7231
mtld <noreply@pustefixframework.org>
2008-01-16 12:32:51 +0000

    Added logout to sample bank application

---
+++ src/de/schlund/pfixcore/example/bank/LogoutState.java
@@ -0,0 +1,18 @@
+package de.schlund.pfixcore.example.bank;
+
+import de.schlund.pfixcore.auth.Authentication;
+import de.schlund.pfixcore.workflow.Context;
+import de.schlund.pfixcore.workflow.StateImpl;
+import de.schlund.pfixxml.PfixServletRequest;
+import de.schlund.pfixxml.ResultDocument;
+
+public class LogoutState extends StateImpl {
+
+    public ResultDocument getDocument(Context context, PfixServletRequest req) throws Exception {;
+        Authentication auth=context.getAuthentication();
+        auth.revokeRole("ACCOUNT");
+        auth.revokeRole("UNRESTRICTED");
+        return new ResultDocument();
+    }
+    
+}
