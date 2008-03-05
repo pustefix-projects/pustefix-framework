c040b7e8cdb8a22faaa389adbbb5a2ac6a8a2f3a
mtld <noreply@pustefixframework.org>
2008-03-05 11:02:11 +0000

    mandatory ContextResource derivation of ContextResource interface

--- src/de/schlund/pfixcore/example/webservices/ContextData.java
+++ src/de/schlund/pfixcore/example/webservices/ContextData.java
@@ -1,6 +1,8 @@
 package de.schlund.pfixcore.example.webservices;
 
-public interface ContextData {
+import de.schlund.pfixcore.workflow.ContextResource;
+
+public interface ContextData extends ContextResource {
     
     public String exchangeData(String data,int strSize) throws Exception;
     public String[] exchangeDataArray(String[] data,int arrSize,int strSize) throws Exception;
