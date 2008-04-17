7705e4560e2392834b6f2ac1379d3d0d8a1ed0b1
jenstl <noreply@pustefixframework.org>
2008-04-17 21:07:05 +0000

    source code formatting

--- pfixcore/src/de/schlund/pfixcore/example/ContextEncodingTest.java
+++ pfixcore/src/de/schlund/pfixcore/example/ContextEncodingTest.java
@@ -7,7 +7,7 @@ import de.schlund.pfixcore.workflow.ContextResource;
 /**
  * @author mleidig@schlund.de
  */
-public interface ContextEncodingTest  extends ContextResource {
+public interface ContextEncodingTest extends ContextResource {
     
     public String getText();
     public void setText(String text);
--- pfixcore/src/de/schlund/pfixcore/example/ContextEncodingTestImpl.java
+++ pfixcore/src/de/schlund/pfixcore/example/ContextEncodingTestImpl.java
@@ -16,73 +16,71 @@ import de.schlund.pfixxml.ResultDocument;
  */
 public class ContextEncodingTestImpl implements ContextEncodingTest {
 
-    private final static String DEFAULT_TEXT="abcdäöüß";
-    
-    private String text=DEFAULT_TEXT;
-    private String encoding="";
-    private File file;
-    
+    private final static String DEFAULT_TEXT = "abcdäöüß";
+    private String              text         = DEFAULT_TEXT;
+    private String              encoding     = "";
+    private File                file;
+
     public String getText() {
-    	return text;
+        return text;
     }
-    
+
     public void setText(String text) {
-    	this.text=text;
+        this.text = text;
     }
-    
+
     public File getFile() {
         return file;
     }
-    
+
     public void setFile(File file) {
-        this.file=file;
+        this.file = file;
     }
-    
+
     public String getEncoding() {
-    	return encoding;
+        return encoding;
     }
-    
+
     public void setEncoding(String encoding) {
-    	this.encoding=encoding;
+        this.encoding = encoding;
     }
-    
-    public void init(Context ctx) {
+
+    public void init(Context ctx) {}
+
+    public void insertStatus(ResultDocument resDoc, Element elem) {
+        ResultDocument.addTextChild(elem, "encoding", encoding);
+        ResultDocument.addTextChild(elem, "original", text);
+        if (file != null) ResultDocument.addTextChild(elem, "file", file.getAbsolutePath());
+        try {
+            String utfEnc = URLEncoder.encode(text, "UTF-8");
+            ResultDocument.addTextChild(elem, "urlenc-utf", utfEnc);
+            String isoEnc = URLEncoder.encode(text, "ISO-8859-1");
+            ResultDocument.addTextChild(elem, "urlenc-iso", isoEnc);
+        } catch (UnsupportedEncodingException x) {}
+        ResultDocument.addObject(elem, "alphabet", new RussianAlphabet());
     }
-    
-    public void insertStatus(ResultDocument resDoc,Element elem) {
-    	ResultDocument.addTextChild(elem,"encoding",encoding);
-    	ResultDocument.addTextChild(elem,"original",text);
-        if(file!=null) ResultDocument.addTextChild(elem,"file",file.getAbsolutePath());
-    	try {
-    	    String utfEnc=URLEncoder.encode(text,"UTF-8");
-    	    ResultDocument.addTextChild(elem,"urlenc-utf",utfEnc);
-    	    String isoEnc=URLEncoder.encode(text,"ISO-8859-1");
-    	    ResultDocument.addTextChild(elem,"urlenc-iso",isoEnc);
-    	} catch(UnsupportedEncodingException x) {}
-    	ResultDocument.addObject(elem,"alphabet",new RussianAlphabet());
-    }
-    
+
     public class RussianAlphabet {
-        
-        private String description;
+
+        private String       description;
         private List<String> characters;
-        
+
         public RussianAlphabet() {
-            description="Basic Russian Alphabet";
-            characters=new ArrayList<String>();
-            for(char ch='\u0410';ch<'\u0430';ch++) {
-                characters.add(ch+" "+Character.toLowerCase(ch));
+            description = "Basic Russian Alphabet";
+            characters = new ArrayList<String>();
+            for (char ch = '\u0410'; ch < '\u0430'; ch++) {
+                characters.add(ch + " " + Character.toLowerCase(ch));
             }
         }
-        
+
         public String getDescription() {
             return description;
         }
-        
+
         public List<String> getCharacters() {
             return characters;
         }
-        
+
     }
-    
+
 }
