4228734d74d4aded0b39c0d9c0713b63b0f6e168
jenstl <noreply@pustefixframework.org>
2008-04-18 10:14:11 +0000

    source code formatting

--- pfixcore/src/de/schlund/pfixcore/example/bank/BankApplication.java
+++ pfixcore/src/de/schlund/pfixcore/example/bank/BankApplication.java
@@ -5,20 +5,20 @@ import de.schlund.pfixcore.example.bank.model.BankInMemoryDAO;
 
 public class BankApplication {
 
-	private static BankApplication instance=new BankApplication();
-	
-	private BankDAO bankDAO;
-	
-	public BankApplication() {
-		bankDAO=new BankInMemoryDAO();
-	}
-	
-	public static BankApplication getInstance() {
-		return instance;
-	}
-	
-	public BankDAO getBankDAO() {
-		return bankDAO;
-	}
-	
+    private static BankApplication instance = new BankApplication();
+
+    private BankDAO                bankDAO;
+
+    public BankApplication() {
+        bankDAO = new BankInMemoryDAO();
+    }
+
+    public static BankApplication getInstance() {
+        return instance;
+    }
+
+    public BankDAO getBankDAO() {
+        return bankDAO;
+    }
+
 }
