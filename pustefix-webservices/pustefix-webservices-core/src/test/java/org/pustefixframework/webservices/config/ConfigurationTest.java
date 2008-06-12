0c45658fcf1605704420ccc7e96edc989b83fb67
mtld <noreply@pustefixframework.org>
2008-06-12 16:21:28 +0000

    move unittest from pfixcore

---
+++ pustefix-webservices/pustefix-webservices-core/src/test/java/org/pustefixframework/webservices/config/ConfigurationTest.java
@@ -0,0 +1,29 @@
+package org.pustefixframework.webservices.config;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+
+import junit.framework.TestCase;
+import de.schlund.pfixxml.config.GlobalConfigurator;
+import de.schlund.pfixxml.resources.FileResource;
+import de.schlund.pfixxml.resources.ResourceUtil;
+
+public class ConfigurationTest extends TestCase {
+
+    @Override
+    protected void setUp() throws Exception {
+        GlobalConfigurator.setDocroot(new File("src/test/resources").getAbsoluteFile().getAbsolutePath());
+    }
+    
+    public void testSerialization() throws Exception {
+        FileResource file=ResourceUtil.getFileResourceFromDocroot("conf"+"/"+"webservice.conf.xml");
+        Configuration conf=ConfigurationReader.read(file);
+        ByteArrayOutputStream out=new ByteArrayOutputStream();
+        ConfigurationReader.serialize(conf,out);
+        ByteArrayInputStream in=new ByteArrayInputStream(out.toByteArray());
+        Configuration refConf=ConfigurationReader.deserialize(in);
+        assertEquals(conf,refConf);
+    }
+    
+}
