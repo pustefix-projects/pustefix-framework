f8d8dfa24eda662991d9b219f91eb41ac5d3685f
mtld <noreply@pustefixframework.org>
2008-08-05 15:05:56 +0000

    sign timestamp

--- pfixcore/src/org/pustefixframework/http/dereferer/SignUtil.java
+++ pfixcore/src/org/pustefixframework/http/dereferer/SignUtil.java
@@ -7,7 +7,13 @@ package org.pustefixframework.http.dereferer;
 import de.schlund.pfixxml.util.MD5Utils;
 
 public abstract class SignUtil {
-    public static String signString(String str, String key) {
-        return MD5Utils.hex_md5(str + key, "utf8");
+    
+    public static String signString(String str, long timeStamp, String key) {
+        return MD5Utils.hex_md5(str + timeStamp + key, "utf8");
     }
+    
+    public static String getTimeStamp() {
+        return String.valueOf(System.currentTimeMillis());
+    }
+    
 }
