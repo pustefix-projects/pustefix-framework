905169e2e78d9b5441c69aa88482a7d469dd6b2c
mtld <noreply@pustefixframework.org>
2008-08-08 13:26:43 +0000

    new contextinterceptor configuration

---
+++ pfixcore/src/org/pustefixframework/config/contextxml/parser/ContextInterceptorParsingHandler.java
@@ -0,0 +1,50 @@
+/*
+ * Place license here
+ */
+
+package org.pustefixframework.config.contextxml.parser;
+
+import org.pustefixframework.config.generic.ParsingUtils;
+import org.w3c.dom.Element;
+
+import com.marsching.flexiparse.parser.HandlerContext;
+import com.marsching.flexiparse.parser.ParsingHandler;
+import com.marsching.flexiparse.parser.exception.ParserException;
+
+import de.schlund.pfixcore.workflow.ContextInterceptor;
+import de.schlund.pfixxml.config.impl.ContextXMLServletConfigImpl;
+
+public class ContextInterceptorParsingHandler implements ParsingHandler {
+
+    public void handleNode(HandlerContext context) throws ParserException {
+       
+        Element element = (Element)context.getNode();
+        ParsingUtils.checkAttributes(element, new String[] {"class"}, null);
+        
+        ContextXMLServletConfigImpl config = ParsingUtils.getSingleTopObject(ContextXMLServletConfigImpl.class, context);     
+        
+        String className = element.getAttribute("class").trim();
+        Class<?> clazz;
+        try {
+            clazz = Class.forName(className);
+        } catch (ClassNotFoundException e) {
+            throw new ParserException("Could not load interceptor class " + className, e);
+        }
+        if (!ContextInterceptor.class.isAssignableFrom(clazz)) {
+            throw new ParserException("Context interceptor " + clazz + " does not implement " + ContextInterceptor.class + " interface!");
+        }
+        
+        Element parent = (Element)element.getParentNode();
+        if (parent.getNodeName().equals("start")) {
+            config.getContextConfig().addStartInterceptor(clazz.asSubclass(ContextInterceptor.class));
+        }
+        if (parent.getNodeName().equals("end")) {
+            config.getContextConfig().addEndInterceptor(clazz.asSubclass(ContextInterceptor.class));
+        }
+        if (parent.getNodeName().equals("postrender")) {
+            config.getContextConfig().addPostRenderInterceptor(clazz.asSubclass(ContextInterceptor.class));
+        }
+        
+    }
+
+}
