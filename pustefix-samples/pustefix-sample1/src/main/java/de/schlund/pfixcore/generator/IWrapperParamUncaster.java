31868b28f0994e234ea52d0929ab393a2959e7ac
mlhartme <noreply@pustefixframework.org>
2009-03-06 10:24:03 +0000

    not part of the example

--- pustefix-samples/pustefix-sample1/src/main/java/de/schlund/pfixcore/generator/IWrapperParamUncaster.java
+++
@@ -1,5 +0,0 @@
-package de.schlund.pfixcore.generator;
-
-public interface IWrapperParamUncaster {
-	public String[] uncastValue(Object[] objArray);
-}
--- pustefix-samples/pustefix-sample1/src/main/java/de/schlund/pfixcore/oxm/impl/AnnotationAware.java
+++
@@ -1,9 +0,0 @@
-package de.schlund.pfixcore.oxm.impl;
-
-import java.lang.annotation.Annotation;
-
-public interface AnnotationAware {
-	
-	public void setAnnotation(Annotation annotation);
-
-}
