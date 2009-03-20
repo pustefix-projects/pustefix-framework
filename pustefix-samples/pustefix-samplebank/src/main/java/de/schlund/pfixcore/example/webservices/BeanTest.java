9ad66397a47e7de670f5319cf0a285bd53cb840f
mlhartme <noreply@pustefixframework.org>
2009-03-20 14:40:15 +0000

    remove unused code from samplebank

--- pustefix-samples/pustefix-samplebank/src/main/java/de/schlund/pfixcore/example/webservices/BeanTest.java
+++
@@ -1,8 +0,0 @@
-package de.schlund.pfixcore.example.webservices;
-
-public interface BeanTest {
-
-    public WeirdBean echoWeird(WeirdBean bean);
-    public WeirdBeanSub echoWeirdSub(WeirdBeanSub bean);
-    
-}
