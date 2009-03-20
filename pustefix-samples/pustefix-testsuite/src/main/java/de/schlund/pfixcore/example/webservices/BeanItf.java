155225e054e705b24c5afba0e4c24da5be1effce
mlhartme <noreply@pustefixframework.org>
2009-03-20 15:32:14 +0000

    dump non-testsuite parts

--- pustefix-samples/pustefix-testsuite/src/main/java/de/schlund/pfixcore/example/webservices/BeanItf.java
+++
@@ -1,8 +0,0 @@
-package de.schlund.pfixcore.example.webservices;
-
-public interface BeanItf {
-
-    public String getText();
-    public void setText(String text);
-    
-}
--- pustefix-samples/pustefix-testsuite/src/main/java/de/schlund/pfixcore/example/webservices/ContextData.java
+++
@@ -1,8 +0,0 @@
-package de.schlund.pfixcore.example.webservices;
-
-public interface ContextData {
-    
-    public String exchangeData(String data,int strSize) throws Exception;
-    public String[] exchangeDataArray(String[] data,int arrSize,int strSize) throws Exception;
-   
-}
