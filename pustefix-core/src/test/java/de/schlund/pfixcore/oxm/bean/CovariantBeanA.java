0420ff39b00e75b3e5f6c63dfb6a5bda97e84971
mtld <noreply@pustefixframework.org>
2009-04-07 15:06:41 +0000

    Fixed method covariance problem with OXM

---
+++ pustefix-core/src/test/java/de/schlund/pfixcore/oxm/bean/CovariantBeanA.java
@@ -0,0 +1,20 @@
+package de.schlund.pfixcore.oxm.bean;
+
+/**
+ * 
+ * @author mleidig@schlund.de
+ *
+ */
+public class CovariantBeanA {
+    
+    private Object value;
+    
+    public Object getValue() {
+        return value;
+    }
+    
+    public void setValue(Object value) {
+        this.value = value;
+    }
+
+}
