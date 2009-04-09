e882eda3e7c583e9a4e92ca1edf86c03078a3d42
mlhartme <noreply@pustefixframework.org>
2009-04-09 12:23:23 +0000

    remove

--- pustefix-core/src/stuff/migration-tool/src/do_migration.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-basepath=`dirname $0`
-saxonpath=`echo $basepath/saxon*.jar`
-classpath=$saxonpath:$basepath/migration-tool.jar
-java -classpath $classpath de.schlund.util.configmigration.MigrationTool
