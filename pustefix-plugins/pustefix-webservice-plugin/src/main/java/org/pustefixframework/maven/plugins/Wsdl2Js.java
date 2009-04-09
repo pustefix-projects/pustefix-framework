b70aa915850a3d3d11b80c1721b8064803340acb
mlhartme <noreply@pustefixframework.org>
2009-04-09 07:32:04 +0000

    fix package name

--- pustefix-plugins/pustefix-webservice-plugin/src/main/java/org/pustefixframework/maven/plugins/Wsdl2Js.java
+++ pustefix-plugins/pustefix-webservice-plugin/src/main/java/org/pustefixframework/maven/plugins/Wsdl2Js.java
@@ -16,7 +16,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
  */
-package org.pustefixframework.maven.generate;
+package org.pustefixframework.maven.plugins;
 
 import java.io.File;
 import java.io.FileInputStream;
--- pustefix-plugins/pustefix-webservice-plugin/src/main/java/org/pustefixframework/maven/plugins/Wsdl2JsException.java
+++ pustefix-plugins/pustefix-webservice-plugin/src/main/java/org/pustefixframework/maven/plugins/Wsdl2JsException.java
@@ -16,7 +16,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
  */
-package org.pustefixframework.maven.generate;
+package org.pustefixframework.maven.plugins;
 
 /**
  * Exception class for Javascript stub generation errors
