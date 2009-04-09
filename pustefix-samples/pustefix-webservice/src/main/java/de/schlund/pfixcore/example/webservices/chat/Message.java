c944bcd1c2de670f5701e3bcedf1a70f74200148
mlhartme <noreply@pustefixframework.org>
2009-04-09 14:54:06 +0000

    copyright/license fixes

--- pustefix-samples/pustefix-webservice/src/main/java/de/schlund/pfixcore/example/webservices/chat/Message.java
+++ pustefix-samples/pustefix-webservice/src/main/java/de/schlund/pfixcore/example/webservices/chat/Message.java
@@ -1,8 +1,19 @@
 /*
- * Created on 09.10.2004
+ * This file is part of Pustefix.
  *
- * To change the template for this generated file go to
- * Window - Preferences - Java - Code Generation - Code and Comments
+ * Pustefix is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Pustefix is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Pustefix; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 package de.schlund.pfixcore.example.webservices.chat;
 
