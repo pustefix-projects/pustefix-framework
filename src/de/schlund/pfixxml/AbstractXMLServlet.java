cdb4adcdf421e3bde7fc21596072cd59658cac45
mtld <noreply@pustefixframework.org>
2009-06-25 10:14:38 +0000

    caching header fix

--- src/de/schlund/pfixxml/AbstractXMLServlet.java
+++ src/de/schlund/pfixxml/AbstractXMLServlet.java
@@ -510,9 +510,12 @@ public abstract class AbstractXMLServlet extends ServletManager {
                 }
                 res.setHeader(key, val);
             }
-        } else {
-            // set some default values to force generating new requests every time...
+        }
+        // set some default values to force generating new requests every time...
+        if(!headers.containsKey("Expires")) {
             res.setHeader("Expires", "Mon, 05 Jul 1970 05:07:00 GMT");
+        }
+        if(!headers.containsKey("Cache-Control")) {    
             res.setHeader("Cache-Control", "private");
         }
         // Check if a content type was supplied
