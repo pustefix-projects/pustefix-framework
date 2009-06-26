043030bacd2a02e43b3c6511aaf4d7bde823a3b4
mtld <noreply@pustefixframework.org>
2009-06-26 13:30:42 +0000

    fixed 'too many open files' problem

---
+++ tests/junit/de/schlund/pfixcore/util/JarFileURLConnectionTest.java
@@ -0,0 +1,92 @@
+package de.schlund.pfixcore.util;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.net.JarURLConnection;
+import java.net.URL;
+import java.util.zip.ZipException;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+public class JarFileURLConnectionTest extends TestCase {
+    
+    private File testModuleFile;
+    
+    @Override
+    protected void setUp() throws Exception {
+        File dir = new File("lib");
+        File[] files = dir.listFiles();
+        for(File file:files) {
+            if(file.isFile() && file.getName().contains("sample-module-A")) {
+                testModuleFile = file;
+                break;
+            }
+        }
+    }
+
+    public void testJarFileURL() throws Exception {
+        String urlStr = "jar:"+testModuleFile.toURI().toString()+"!/";
+        URL url = new URL(urlStr);   
+        JarURLConnection con = (JarURLConnection)url.openConnection();
+        JarURLConnection conFile = new JarFileURLConnection(url);
+        assertEquals(con.getLastModified(), con.getLastModified());
+        assertNotNull(con.getJarFile());
+        assertNotNull(conFile.getJarFile());
+    }
+    
+    public void testNonExistingJarFileURL() throws Exception {
+        String urlStr = "jar:"+testModuleFile.toURI().toString()+"xxx!/";
+        URL url = new URL(urlStr);   
+        JarURLConnection con = (JarURLConnection)url.openConnection();
+        JarURLConnection conFile = new JarFileURLConnection(url);
+        Assert.assertEquals(con.getLastModified(), con.getLastModified());
+        ZipException error = null;
+        try {
+            con.getJarFile();
+        } catch(ZipException x) {
+            error = x;
+        }
+        assertNotNull(error);
+        error = null;
+        try {
+            conFile.getJarFile();
+        } catch(ZipException x) {
+            error = x;
+        }
+        assertNotNull(error);
+    }
+    
+    public void testJarEntryURL() throws Exception {
+        String urlStr = "jar:"+testModuleFile.toURI().toString()+"!/PUSTEFIX-INF/txt/common.xml";
+        URL url = new URL(urlStr);   
+        JarURLConnection con = (JarURLConnection)url.openConnection();
+        JarURLConnection conFile = new JarFileURLConnection(url);
+        assertEquals(con.getLastModified(), con.getLastModified());
+        assertNotNull(con.getJarEntry());
+        assertNotNull(conFile.getJarEntry());
+    }
+    
+    public void testNonExistingJarEntryURL() throws Exception {
+        String urlStr = "jar:"+testModuleFile.toURI().toString()+"!/PUSTEFIX-INF/txt/commonXXX.xml";
+        URL url = new URL(urlStr);   
+        JarURLConnection con = (JarURLConnection)url.openConnection();
+        JarURLConnection conFile = new JarFileURLConnection(url);
+        assertEquals(con.getLastModified(), con.getLastModified());
+        FileNotFoundException error = null;
+        try {
+            con.getJarEntry();
+        } catch(FileNotFoundException x) {
+            error = x;
+        }
+        assertNotNull(error);
+        error = null;
+        try {
+            conFile.getJarEntry();
+        } catch(FileNotFoundException x) {
+            error = x;
+        }
+        assertNotNull(error);
+    }
+    
+}
