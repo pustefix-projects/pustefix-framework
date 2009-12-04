1135c46355f3b846ed3af2fe2c7307d7d12107ac
mtld <noreply@pustefixframework.org>
2009-12-04 16:32:48 +0000

    migrated pustefix-webservice application to 1.x

--- pustefix-samples/pustefix-webservice/src/main/resources/PUSTEFIX-INF/script/calc-pfxsoap.js
+++ pustefix-samples/pustefix-webservice/src/main/resources/PUSTEFIX-INF/script/calc-pfxsoap.js
@@ -1,6 +1,6 @@
 var wsCalc=new WS_Calculator();
 //var jwsCalc=new pfx.ws.json.DynamicProxy("Calculator");
-var jwsCalc=new JWS_Calculator();
+//var jwsCalc=new JWS_Calculator();
 //jwsCalc.setDebug(true);
 //alert(jwsCalc._protocol);
 //alert(jwsCalc.add(2,4));
@@ -17,7 +17,8 @@ function serviceCallback(result,reqID,exception) {
 function serviceCall(method,val1,val2) {
    timer.reset();
 	timer.start();
-	var ws=soapEnabled()?wsCalc:jwsCalc;
+	//var ws=soapEnabled()?wsCalc:jwsCalc;
+	var ws = wsCalc;
 	var param1=parseInt(val1);
 	var param2=parseInt(val2);
 	if(method=="add") ws.add(param1,param2,serviceCallback);
