6427736dbb120d7635ab8ffadbceb65294dae965
mtld <noreply@pustefixframework.org>
2010-04-14 16:54:19 +0000

    added Pustefix archetype for basic application

---
+++ pustefix-archetypes/pustefix-archetype-basic/src/main/resources/archetype-resources/src/main/java/context/User.java
@@ -0,0 +1,18 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.context;
+
+public class User {
+
+    private String name;
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
