683782ac8c28929663c7aeca759cb7ab5efefd5a
mtld <noreply@pustefixframework.org>
2010-05-03 15:06:00 +0000

    added modular application sample

---
+++ pustefix-samples/pustefix-sample-modular/webmodule/src/main/java/org/pustefixframework/samples/modular/login/web/ContextUser.java
@@ -0,0 +1,25 @@
+package org.pustefixframework.samples.modular.login.web;
+
+
+public class ContextUser {
+
+	private int userId;
+        private String userName;
+	
+	public int getUserId() {
+		return userId;
+	}
+	
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	
+	public String getUserName() {
+		return userName;
+	}
+	
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+}
---
+++ pustefix-samples/pustefix-sample-modular/webmodule/src/main/java/org/pustefixframework/samples/modular/login/web/LoginHandler.java
@@ -0,0 +1,28 @@
+package org.pustefixframework.samples.modular.login.web;
+
+import de.schlund.pfixcore.generator.IHandler;
+import de.schlund.pfixcore.generator.IWrapper;
+import de.schlund.pfixcore.workflow.Context;
+
+public class LoginHandler implements IHandler {
+
+	public void handleSubmittedData(Context context, IWrapper wrapper) throws Exception {
+	}
+	
+	public boolean isActive(Context context) throws Exception {
+		return true;
+	}
+	
+	public boolean needsData(Context context) throws Exception {
+		return false;
+	}
+	
+	public boolean prerequisitesMet(Context context) throws Exception {
+		return true;
+	}
+	
+	public void retrieveCurrentStatus(Context context, IWrapper wrapper) throws Exception {
+	}
+	
+	
+}
