2052e10dd169768d26e7a7ece43b479814b9eff5
redtoxicfrog <noreply@pustefixframework.org>
2011-02-07 15:35:12 +0000

    bugfix for pfixlog parsing with windows

--- pustefix-core/src/main/java/de/schlund/pfixcore/util/FileUriTransformer.java
+++ pustefix-core/src/main/java/de/schlund/pfixcore/util/FileUriTransformer.java
@@ -7,9 +7,9 @@ public class FileUriTransformer {
 	/**
 	 * Build URI without the windows root (e.g get rid of C:),
 	 * because javax.xml.transform.Transformer can't handle it
-	 * and add the "file://" prefix.
+	 * and add the "file://" prefix when needed.
 	 * 
-	 * e.g.: 
+	 * e.g.:
 	 * C:/workspace/pustefix-core/test/resources/file.xml and
 	 * /workspace/pustefix-core/test/resources/file.xml will return
 	 * the same result: file:///workspace/pustefix-core/test/resources/file.xml
@@ -23,10 +23,14 @@ public class FileUriTransformer {
 	}
 	
 	public static String getFileUriWithoutWindowsDriveSpecifier(String absolutePath) {
+		return "file://" + getUriWithoutWindowsDriveSpecifier(absolutePath);
+	}
+	
+	public static String getUriWithoutWindowsDriveSpecifier(String absolutePath) {
 		if (absolutePath.charAt(1) == ':') {
 			absolutePath = absolutePath.substring(2).replace(
 					File.separatorChar, '/');
 		}
-		return "file://" + absolutePath;
+		return absolutePath;
 	}
 }
