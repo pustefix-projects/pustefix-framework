6fe1df8c9f718767c77872a3a0feb7fab772d552
mtld <noreply@pustefixframework.org>
2011-07-21 09:33:18 +0000

    remove logs written during integration test

--- pustefix-samples/pustefix-sample1/src/test/java/de/schlund/pfixcore/example/SessionMockTest.java
+++ pustefix-samples/pustefix-sample1/src/test/java/de/schlund/pfixcore/example/SessionMockTest.java
@@ -4,6 +4,10 @@ import java.io.File;
 
 import junit.framework.TestCase;
 
+import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PatternLayout;
 import org.pustefixframework.container.spring.beans.PustefixWebApplicationContext;
 import org.pustefixframework.test.PustefixWebApplicationContextLoader;
 import org.springframework.mock.web.MockHttpServletRequest;
@@ -11,10 +15,13 @@ import org.springframework.mock.web.MockHttpSession;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
+import de.schlund.pfixxml.util.FileUtils;
+
 public class SessionMockTest extends TestCase {
 
     public void test() {
         
+        try  {
         PustefixWebApplicationContextLoader loader = new PustefixWebApplicationContextLoader(new File("src/main/webapp"));
         String[] locations = {"docroot:/WEB-INF/project.xml", "docroot:/WEB-INF/spring.xml"};
         PustefixWebApplicationContext appContext = (PustefixWebApplicationContext) loader.loadContext(locations);
@@ -26,6 +33,9 @@ public class SessionMockTest extends TestCase {
 
         TestData data = (TestData)appContext.getBean("testdata");
         data.getText();
+        } finally {
+            FileUtils.delete(new File("src/main/webapp/log"));
+        }
     }
     
 }
