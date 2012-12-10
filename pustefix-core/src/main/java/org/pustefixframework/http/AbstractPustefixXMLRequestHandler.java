83bdbc1ff4337735dc512e440328b350ff62575e
mtld <noreply@pustefixframework.org>
2012-12-10 18:04:02 +0000

    sticky render include session workaround

--- pustefix-core/src/main/java/org/pustefixframework/http/AbstractPustefixXMLRequestHandler.java
+++ pustefix-core/src/main/java/org/pustefixframework/http/AbstractPustefixXMLRequestHandler.java
@@ -273,6 +273,10 @@ public abstract class AbstractPustefixXMLRequestHandler extends AbstractPustefix
         boolean    doreuse = false;
         if (spdoc != null) {
             doreuse = true;
+        } else if(preq.getRequestParam("__render_href") != null && preq.getRequestParam("__reuse") != null) {
+        	//TODO: sticky session loss workaround for render includes
+        	res.sendError(HttpServletResponse.SC_GONE);
+        	return;
         }
         
         // Check if session has been scheduled for invalidation
