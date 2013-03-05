a2c3449126b135b292fe8741567a4135ada89b1e
mtld <noreply@pustefixframework.org>
2013-03-05 14:54:01 +0000

    ignore unknown actions instead of throwing exception

--- pustefix-core/src/main/java/de/schlund/pfixcore/workflow/context/RequestContextImpl.java
+++ pustefix-core/src/main/java/de/schlund/pfixcore/workflow/context/RequestContextImpl.java
@@ -413,7 +413,7 @@ public class RequestContextImpl implements Cloneable, AuthorizationInterceptor {
                     }
                 }
                 if (action == null) {
-                    throw new UnknownActionException(actionname.getValue(), currentpagerequest.getName());
+                    LOG.warn("Page '" + currentpagerequest.getName() + "' has been called with unknown action.");
                 }
             } else {
                 LOG.warn("Page " + currentpagerequest.getName() + " has been called with action, but isn't configured.");
