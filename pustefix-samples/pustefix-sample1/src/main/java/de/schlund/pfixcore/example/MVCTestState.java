101a8675edfb29c5be4557cde085db3e2967593a
mtld <noreply@pustefixframework.org>
2015-01-09 11:25:26 +0000

    added custom Filter implementation example

--- pustefix-samples/pustefix-sample1/src/main/java/de/schlund/pfixcore/example/MVCTestState.java
+++ pustefix-samples/pustefix-sample1/src/main/java/de/schlund/pfixcore/example/MVCTestState.java
@@ -1,30 +1,37 @@
 package de.schlund.pfixcore.example;
 
-import org.pustefixframework.web.mvc.filter.Filter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import de.schlund.pfixcore.workflow.app.DefaultIWrapperState;
 
-
+@SessionAttributes("myfilter")
 public class MVCTestState extends DefaultIWrapperState {
 
     @Autowired
     private ContextData contextData;
 
+    @ModelAttribute("myfilter")
+    public DataFilter getDataFilter() {
+        //Setup initial filter, e.g. get settings from preference storage
+        DataFilter dataFilter = new DataFilter();
+        //dataFilter.getFilter().setEnabled(true);
+        return dataFilter;
+    }
+    
     @RequestMapping("/mvctest")
-    public void list(Model model, Pageable pageable, Filter filter) {
-
+    public void list(Model model, Pageable pageable, @ModelAttribute("myfilter") DataFilter filter) {
         if(pageable.getSort() == null) {
             pageable = new PageRequest(0, 10, new Sort(Sort.Direction.ASC, "id"));
         }
-        
-        model.addAttribute("data", contextData.getDataList(pageable, filter));
+        model.addAttribute("data", contextData.getDataList(pageable, filter.getPropertyFilter()));
         if(filter != null) {
             model.addAttribute("filter", filter);
         }
@@ -32,7 +39,6 @@ public class MVCTestState extends DefaultIWrapperState {
 
     @RequestMapping("/mvctest/data/{dataId}")
     public void details(@PathVariable long dataId, Model model) {
-
         model.addAttribute("data", contextData.getData(dataId));
     }
 
