10d3974e7980201e1eabe8de2096da4009dcdbe4
mtld <noreply@pustefixframework.org>
2015-02-27 15:54:54 +0000

    support currentPageFlow mocking

--- pustefix-core/src/main/java/org/pustefixframework/test/MockContext.java
+++ pustefix-core/src/main/java/org/pustefixframework/test/MockContext.java
@@ -77,6 +77,7 @@ public class MockContext implements Context {
     private Authentication authentication;
     private PageRequest pageRequest;
     private PageRequestStatus pageRequestStatus;
+    private PageFlow currentPageFlow;
     private List<StatusCodeInfo> messages   = new ArrayList<StatusCodeInfo>();
     private Map<Object,Properties> contextResourceProps = new HashMap<Object,Properties>();
     private Set<String> pageNeedsDataSet = new HashSet<String>();
@@ -121,7 +122,11 @@ public class MockContext implements Context {
     }
 
     public PageFlow getCurrentPageFlow() {
-        throw new UnsupportedOperationException("Method isn't supported any more");
+        return currentPageFlow; 
+    }
+
+    public void setCurrentPageFlow(PageFlow currentPageFlow) {
+        this.currentPageFlow = currentPageFlow;
     }
     
     public PageRequestStatus getCurrentStatus() {
